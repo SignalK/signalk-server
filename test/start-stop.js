61d3d392d997d9bda425d23907de18f1503a7148
Fabian Tollenaar <fabian@starting-point.nl>
2014-12-11 11:32:07 +0100

    implemented killign clients logic

--- test/start-stop.js
+++ test/start-stop.js
@@ -1,18 +1,18 @@
 var Server = require('../lib');
 
-console.log('1. Instantiating instance...');
+console.log('1. INSTANTIATING instance...');
 var server = new Server({ settings: 'volare-file-settings.json' });
 
-console.log('2. Starting server...');
+console.log('2. STARTING server...');
 server.start();
 
 setTimeout(function() {
-  console.log('3. Stopping server...');
+  console.log('3. STOPPING server...');
   server.stop();
 }, 10000);
 
 
 setTimeout(function() {
-  console.log('4. Starting server...');
+  console.log('4. STARTING server...');
   server.start();
-}, 90000);
+}, 30000);
