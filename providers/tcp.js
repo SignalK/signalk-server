f855ef489b3e4e78272d6301e5dbd08b521f7130
Fabian Tollenaar <fabian@starting-point.nl>
2015-06-11 23:55:05 +0200

    refactored the TcpStream functionality

--- providers/tcp.js
+++ providers/tcp.js
@@ -14,40 +14,120 @@
  * limitations under the License.
  */
 
-var net = require('net');
-var PassThrough = require('stream').PassThrough;
+var net       = require('net')
+  , Transform = require('stream').Transform
+  , debug     = require('debug')('signalk-provider-tcp')
+;
 
-function EndIgnoringPassThrough() {
-  PassThrough.call(this);
-}
-
-require('util').inherits(EndIgnoringPassThrough, PassThrough);
-EndIgnoringPassThrough.prototype.end = function() {}
-
-var TcpStream = function(options) {
-  this.options = options;
-  this.keepRunning = true;
-}
-
-TcpStream.prototype.pipe = function(pipeTo) {
-  this.pipeTo = pipeTo;
-  this.endIgnoringPassThrough = new EndIgnoringPassThrough();
-  this.endIgnoringPassThrough.pipe(pipeTo);
-  this.startStream();
-}
-
-TcpStream.prototype.startStream = function() {
-  if (this.keepRunning) {
-    this.client = net.connect(this.options);
-    this.client.on('end', this.startStream.bind(this));
-    this.client.pipe(this.endIgnoringPassThrough);
+function TcpStream(options) {
+  if(!(this instanceof TcpStream)) {
+    return new TcpStream(options);
   }
+
+  Transform.call(this, options);
+
+  this.options    = options;
+  this.reconnect  = options.reconnect || true;
+  this.socket     = null;
+  this.retries    = 0;
+  this.maxRetries = options.maxRetries || 10;
+
+  this.__reset    = null;
+  this.__timeout  = null;
+  this.__last     = -1;
+
+  this.start();
+
+  this.on('error', function(err) {
+    debug('TcpStream', err);
+  });
 }
+
+require('util').inherits(TcpStream, Transform);
+
+TcpStream.prototype.handleTimeout = function() {
+  if((Date.now() - this.__last) > 90000) {
+    debug('Connection timed out. Resetting.');
+    this.start();
+  } else {
+    if(this.__timeout !== null) {
+      clearTimeout(this.__timeout);
+    }
+
+    this.__timeout = setTimeout(this.handleTimeout.bind(this), 120000);
+  }
+};
+
+TcpStream.prototype.start = function() {
+  if(this.socket !== null) {
+    this.socket.unpipe(this);
+    this.socket.removeAllListeners('error');
+    this.socket.removeAllListeners('close');
+    this.socket.removeAllListeners('end');
+    this.socket.destroy();
+    this.socket = null;
+  }
+
+  if(this.reconnect === true && this.socket === null) {
+    if(this.__timeout !== null) {
+      clearTimeout(this.__timeout);
+    }
+
+    this.socket = net.connect(this.options);
+
+    if(this.__timeout !== null) {
+      clearTimeout(this.__timeout);
+    }
+
+    this.__timeout = setTimeout(this.handleTimeout.bind(this), 120000);
+
+    this.socket.on('close', function() {
+      if(this.__reset === null) {
+        this.start();
+      }
+    }.bind(this));
+
+    this.socket.on('error', function(err) {
+      debug(err.message);
+      this.retries++;
+
+      if(this.retries < this.maxRetries) {
+        debug('Retrying... ' + this.retries + ' / ' + this.maxRetries);
+        this.start();
+      } else {
+        debug('Out of retries. Retrying in a minute.');
+        if(this.__reset === null) {
+          this.__reset = setTimeout(function() {
+            this.__timeout = null;
+            this.maxRetries = 1;
+            this.retries = 0;
+            this.start();
+          }.bind(this), 30000);
+        }
+      }
+    }.bind(this));
+
+    this.socket.pipe(this);
+  }
+};
+
+TcpStream.prototype._transform = function(chunk, encoding, done) {
+  // console.log(chunk.toString('utf8'));
+  this.__last = Date.now();
+  this.push(chunk);
+  done();
+};
 
 TcpStream.prototype.end = function() {
-  this.keepRunning = false;
-  this.pipeTo.end();
-  this.client.close();
-}
+  this.reconnect = false;
+  this.start();
+};
+
+/* TESTING *
+var s = new TcpStream({
+  host: '127.0.0.1',
+  port: 1337
+});
+//*/
 
 module.exports = TcpStream;
