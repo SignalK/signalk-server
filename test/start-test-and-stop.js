5b1b81fffb63d26d53ce00f3e3cbe60039aeb47c
Teppo Kurki <tjk@iki.fi>
2015-08-15 17:51:31 +0300

    Restructure test It now starts server, posts two deltas and asserts some tree paths (values, source pgn) and stops the server.

---
+++ test/start-test-and-stop.js
@@ -0,0 +1,83 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+var rp = require('request-promise');
+
+
+
+var delta = {
+  "context": "vessels.123456789",
+  "updates": [
+  {
+    "source": {
+      "pgn": "128275",
+      "device" : "/dev/actisense",
+      "timestamp": "2014-05-03T09:14:11.000Z",
+      "src": "115"
+    },
+    "values": [
+    { "path": "navigation.logTrip","value": 43374 },
+    { "path": "navigation.log","value": 17404540 }
+    ]
+  },
+  {
+    "source": {
+      "device" : "/dev/actisense",
+      "timestamp":"2014-05-03T09:14:11.000Z",
+      "src":"115",
+      "pgn":"128267"
+    },
+
+    "values": [
+    { "path": "navigation.courseOverGroundTrue", "value": 172.9 },
+    { "path": "navigation.speedOverGround", "value": 3.85 }
+    ]
+  }
+  ]
+}
+
+
+describe('Server', function() {
+  it('handles two deltas with signalk path', function(done) {
+    var fp = require("find-free-port")
+    fp(3000, function(err, freePort) {
+      var Server = require('../lib');
+
+      var server = new Server({
+        settings: './test/server-test-settings.json',
+        port: freePort
+      });
+
+      server.start();
+
+      var host = 'http://localhost:' + freePort;
+      var deltaUrl = host + '/delta';
+      var restUrl = host + '/signalk/api/v1/';
+      var treeAfterFirstDelta;
+      var treeAfterSecondDelta;
+
+      rp({ url: deltaUrl, method: 'POST', json: delta })
+      .then(function(body) {
+        return rp({ url: restUrl, method: 'GET'})
+      }).then(function(body) {
+        treeAfterFirstDelta = JSON.parse(body);
+
+        delta.updates[0].values[0].value = 1;
+        delta.updates[0].source.src = '116';
+        return rp({ url: deltaUrl, method: 'POST', json: delta })
+      }).then(function(body) {
+        return rp({ url: restUrl, method: 'GET'})
+      }).then(function(body) {
+        treeAfterSecondDelta = JSON.parse(body);
+      }).finally(function() {
+        treeAfterFirstDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.value', 43374);
+        treeAfterFirstDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.source.src', '115');
+        treeAfterSecondDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.value', 1);
+        treeAfterSecondDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.source.src', '116');
+        server.stop();
+        done();        
+      });
+    })
+  })
+});
