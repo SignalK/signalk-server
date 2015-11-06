50b69060f85a8321eb3964b6dfd423527f7a8d3c
Teppo Kurki <teppo.kurki@iki.fi>
2015-11-06 09:31:10 +0200

    Add delta from a 'second sensor'

--- test/start-test-and-stop.js
+++ test/start-test-and-stop.js
@@ -28,7 +28,6 @@ var delta = {
       "src":"115",
       "pgn":"128267"
     },
-
     "values": [
     { "path": "navigation.courseOverGroundTrue", "value": 172.9 },
     { "path": "navigation.speedOverGround", "value": 3.85 }
@@ -56,6 +55,7 @@ describe('Server', function() {
       var restUrl = host + '/signalk/api/v1/';
       var treeAfterFirstDelta;
       var treeAfterSecondDelta;
+      var treeAfterOtherSourceDelta;
 
       rp({ url: deltaUrl, method: 'POST', json: delta })
       .then(function(body) {
@@ -70,11 +70,19 @@ describe('Server', function() {
         return rp({ url: restUrl, method: 'GET'})
       }).then(function(body) {
         treeAfterSecondDelta = JSON.parse(body);
-      }).finally(function() {
+
+        delta.updates[0].src = 116
+        return rp({ url: deltaUrl, method: 'POST', json: delta })
+      }).then(function(body) {
+        return rp({ url: restUrl, method: 'GET'})
+      }).then(function(body) {
+        treeAfterOtherSourceDelta = JSON.parse(body);
+      }).then(function() {
         treeAfterFirstDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.value', 43374);
         treeAfterFirstDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.source.src', '115');
         treeAfterSecondDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.value', 1);
         treeAfterSecondDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.source.src', '116');
+        treeAfterOtherSourceDelta.should.have.deep.property('vessels.123456789.navigation.logTrip.source.src', '115');
         server.stop();
         done();        
       });
