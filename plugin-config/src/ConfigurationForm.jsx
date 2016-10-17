1b8b161d6b03d4a9cfd0c093a53be6077b017b3b
Teppo Kurki <teppo.kurki@iki.fi>
2016-10-17 22:20:33 +0300

    Reorganise, add persistence

--- plugin-config/src/ConfigurationForm.jsx
+++ plugin-config/src/ConfigurationForm.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, {Component} from "react";
+import {render} from "react-dom";
 
 import Form from "react-jsonschema-form";
 
-
-const log = (type) => console.log.bind(console, type);
-
-export default (props) => (
-  <Form schema={props.schema}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")} />
+export default(props) => (
+  <Form
+    schema={props.plugin.schema}
+    formData={props.plugin.data || {}}
+    onSubmit={submitData => {
+      props.onSubmit(submitData.formData)
+    }
+    }
+  />
 )
--- plugin-config/src/main.jsx
+++ plugin-config/src/main.jsx
@@ -1,23 +1,29 @@
 import ConfigurationForm from "./ConfigurationForm.jsx"
 import PluginList from "./PluginList.jsx"
 
+function saveData(id, data) {
+  fetch('/plugins/' + id + "/config", {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({'Content-Type': 'application/json'})
+  }).then((response) => {
+    console.log(response)
+  })
+}
 
 fetch(`/plugins`).then((response) => {
   if (response.status == 200) {
     response.json().then((plugins) => {
       ReactDOM.render(
-      <div className="container">
+        <div className="container">
         {plugins.map((plugin, i) => (
-        <div className="panel panel-default">
-          <div className="panel-body">
-            <ConfigurationForm schema={plugin.schema}/>
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <ConfigurationForm plugin={plugin} onSubmit={saveData.bind(null, plugin.id)}/>
+            </div>
           </div>
-        </div>
         ))}
-
-      </div>,
-        document.getElementById('main')
-      )
+      </div>, document.getElementById('main'))
     })
   } else {
     console.log(response.status)
