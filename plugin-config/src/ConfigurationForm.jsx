09507bbfedbbd908e45c515987a56aec3859c2b3
Teppo Kurki <teppo.kurki@iki.fi>
2016-10-30 19:37:29 +0200

    Add server-driven enabled/disabled state

--- plugin-config/src/ConfigurationForm.jsx
+++ plugin-config/src/ConfigurationForm.jsx
@@ -1,15 +1,28 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
+import React, {Component} from "react"
+import {render} from "react-dom"
+import keys from 'lodash.keys'
 
 import Form from "react-jsonschema-form";
 
-export default(props) => (
-  <Form
-    schema={props.plugin.schema}
+export default(props) => {
+  const schema = JSON.parse(JSON.stringify(props.plugin.schema))
+  const uiSchema = {
+    "ui:order": keys(schema.properties)
+  }
+  uiSchema['ui:order'].unshift('enabled')
+  schema.properties.enabled = {
+    type: "boolean",
+    title: "Active",
+    default: false
+  }
+
+  return <Form
+    schema={schema}
+    uiSchema={uiSchema}
     formData={props.plugin.data || {}}
     onSubmit={submitData => {
       props.onSubmit(submitData.formData)
     }
     }
   />
-)
+}
