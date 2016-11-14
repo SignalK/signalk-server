6b6aaaf479666f9be56927f1841410a2b563236c
Joachim Bakke <github@heiamoss.com>
2016-11-14 14:35:27 +0100

    fixed mdns port change in settings file
    Now all should be ok

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -131,8 +131,8 @@ socket
 
 sudo chmod 755 $socket
 
+sed -i -e "s/env.PORT || [0-9]\+;/env.PORT || $port;/g" $dir/lib/config/config.js
 
+echo "A reboot is recommended"
 
 sudo systemctl daemon-reload
-sudo systemctl enable signalk.service
-sudo systemctl enable signalk.socket
