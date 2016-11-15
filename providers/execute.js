9ef113fb7e58fc15883bb9ec17e8e3f1395b38b0
Teppo Kurki <teppo.kurki@iki.fi>
2016-11-15 08:50:00 +0200

    Format, correct debug topic

--- providers/execute.js
+++ providers/execute.js
@@ -1,26 +1,26 @@
 /*
- * 
+ *
  * prototype-server: An implementation of a Signal K server for boats.
- * Copyright (C) 2014  Fabian Tollenaar <fabian@starting-point.nl>, 
+ * Copyright (C) 2014  Fabian Tollenaar <fabian@starting-point.nl>,
  * Teppo Kurki <teppo.kurki@iki.fi> *et al*.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 var Transform = require('stream').Transform;
-var debug = require('debug')('signalk:n2kAnalyzer');
+var debug = require('debug')('signalk:executor');
 
 function Execute(options) {
   Transform.call(this, {
