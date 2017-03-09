e297a0a49bacdf35e1978629aceee1ab2b0cb050
Teppo Kurki <teppo.kurki@iki.fi>
2017-03-09 20:01:32 +0200

    feature: use can name data to identify source
    The logic here keeps a map from srcs to data received for this source in PGN
    60928. This data is used to add manufacturerCode and uniqueNumber fields to
    each delta update's source object. If there is no 60928 can name data a
    59904 PGN is sent to request the data from the source.
    Deltas that do not have can name data for their src can optionally be
    ignored by configuration option ignoreUnknownSrcs.
    Furthermore this commit changes package.json to signalk-schema version
    #canname, where the canname data is used to generate source paths.

--- providers/n2k-signalk.js
+++ providers/n2k-signalk.js
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- 
+
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,26 +14,48 @@
  * limitations under the License.
  */
 
-var Transform = require('stream').Transform;
-
-var toDelta = require('n2k-signalk').toDelta;
+const Transform = require('stream').Transform
+const toDelta = require('n2k-signalk').toDelta
+const debug = require('debug')('signalk:provider:n2k-signalk')
 
 require('util').inherits(ToSignalK, Transform);
 
-function ToSignalK() {
+function ToSignalK(options) {
   Transform.call(this, {
     objectMode: true
   });
+  this.srcCannames = {}
+  this.app = options.app
+  this.ignoreUnknownSrcs = typeof options.ignoreUnknownSrcs === "undefined" ? false : options.ignoreUnknownSrcs
 }
 
 
-ToSignalK.prototype._transform = function(chunk, encoding, done) {
+ToSignalK.prototype._transform = function(analyzerJson, encoding, done) {
   try {
-    var delta = toDelta(chunk);
-    if (delta && delta.updates[0].values.length > 0) {
-      this.push(delta);
+    if(analyzerJson.pgn === 60928) {
+      this.srcCannames[analyzerJson.src] = analyzerJson.fields
+    } else {
+      var delta = toDelta(analyzerJson);
+      if(delta && delta.updates[0].values.length > 0) {
+        //reduceRight, because we may modify the array while iterating
+        //and remaining indices do not change this way
+        delta.updates.reduceRight((acc, update, i) => {
+          if(update.source && update.source.src && this.srcCannames[update.source.src]) {
+            update.source.manufacturerCode = this.srcCannames[update.source.src]['Manufacturer Code']
+            update.source.uniqueNumber = this.srcCannames[update.source.src]['Unique Number']
+          } else {
+            const isoRequest = new Date().toISOString() + ',6,59904,0,' + analyzerJson.src.toString() + ',3,0,ee,00'
+            debug('ISO Request:' + isoRequest)
+            this.app.emit('nmea2000out', isoRequest)
+            if(this.ignoreUnknownSrcs) {
+              debug("Delta with no can name available ignored:" + JSON.stringify(delta.updates.splice(i, 1)))
+            }
+          }
+        }, [])
+        this.push(delta);
+      }
     }
-  } catch (ex) {
+  } catch(ex) {
     console.error(ex);
   }
   done();
