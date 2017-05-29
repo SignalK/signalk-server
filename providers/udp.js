5bcd44b526a4b3479d425697724919ba9c6b0cb1
Joachim Bakke <github@heiamoss.com>
2017-05-29 23:17:58 +0200

    semantics in comments

--- providers/udp.js
+++ providers/udp.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- /* Usage: This provider takes any TCP stream and passes it to the next provider.
+ /* Usage: This pipeElement takes any TCP stream and passes it to the next pipeElement.
  * It takes the options "host" and "port" and optionally "reconnect" (default true) and "maxRetries" (default 10)
  * Example:
 
