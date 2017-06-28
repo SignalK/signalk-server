835d6edb2edfedc6cac48e93131861f6314396eb
Teppo Kurki <teppo.kurki@iki.fi>
2017-06-28 21:08:17 +0300

    feature: allow multiple events in 0183 out and serial in
    This adds the ability to specify that nmea0183-signalk provider emits sentences as one
    or more events. Furthermore it allows serial provider to listen for multiple events for
    data to be written to serial out.

--- providers/nmea0183-signalk.js
+++ providers/nmea0183-signalk.js
@@ -14,12 +14,19 @@
  * limitations under the License.
  */
 
- /* Usage: this is the pipeElement that transforms NMEA0183 input to Signal K deltas
- * It does not take any options, as selfId and selfType are fetched from app properties
+/* Usage: this is the pipeElement that transforms NMEA0183 input to Signal K deltas
+ * SelfId and selfType are fetched from app properties. Emits sentence data as "nmea0183"
+ * events on app.signalk by default. Furthermore you can use "sentenceEvent" option,
+ * that will cause sentence data to be emitted as events on app. sentenceEvent can
+ * be a string or an array of strings.
+ *
  * Example:
 
  {
    "type": "providers/nmea0183-signalk",
+   "options": {
+     "sentenceEvent": "nmea0183-1"
+   },
    "optionMappings": [
      {
        "fromAppProperty": "selfId",
@@ -34,44 +41,54 @@
 
  */
 
-var Transform = require('stream').Transform;
+const Transform = require("stream").Transform;
+const isArray = require("lodash").isArray;
+
 
 function ToSignalK(options) {
   Transform.call(this, {
     objectMode: true
   });
 
-  this.parser = new(require('nmea0183-signalk').Parser)(options);
+  this.parser = new (require("nmea0183-signalk")).Parser(options);
 
   var that = this;
-  this.parser.on('nmea0183', function(sentence) {
-    that.emit('nmea0183', sentence)
+  this.parser.on("nmea0183", function(sentence) {
+    that.emit("nmea0183", sentence);
   });
-  this.parser.on('delta', function (delta) {
+
+  if (options.sentenceEvent) {
+    (isArray(options.sentenceEvent)
+      ? options.sentenceEvent
+      : [options.sentenceEvent]).forEach(event => {
+      that.parser.on("nmea0183", sentence => options.app.emit(event, sentence));
+    });
+  }
+
+  this.parser.on("delta", function(delta) {
     if (that.timestamp) {
       delta.updates.forEach(update => {
-        update.timestamp = that.timestamp
-      })
+        update.timestamp = that.timestamp;
+      });
     }
     that.push(delta);
   });
 }
 
-require('util').inherits(ToSignalK, Transform);
+require("util").inherits(ToSignalK, Transform);
 
 ToSignalK.prototype._transform = function(chunk, encoding, done) {
   try {
-    if (typeof chunk === 'object' && typeof chunk.line === 'string') {
-      this.timestamp = new Date(Number(chunk.timestamp))
-      this.parser.write(chunk.line + '\n')
+    if (typeof chunk === "object" && typeof chunk.line === "string") {
+      this.timestamp = new Date(Number(chunk.timestamp));
+      this.parser.write(chunk.line + "\n");
     } else {
-      this.parser.write(chunk + '\n');
+      this.parser.write(chunk + "\n");
     }
   } catch (ex) {
     console.error(ex);
   }
   done();
-}
-
+};
 
 module.exports = ToSignalK;
