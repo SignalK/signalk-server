b06b1500b4d955c1869527a3fb8c06dbfcffc3b5
Scott Bender <scott@scottbender.net>
2017-12-20 15:56:21 -0500

    refactor: simple provider refactored by tkurki and syntax errors fixed

--- providers/simple.js
+++ providers/simple.js
@@ -18,45 +18,37 @@ const throttle = require('./throttle')
 
 function Simple (options) {
   Transform.call(this, { objectMode: true })
-  this.options = options
-  this.pipeline = []
 
-  var source = pipeStartByType[options.type]
-
-  if ( !source ) {
-    throw new Error(`invalid type: ${options.type}`)
+  const dataType = options.subOptions.dataType || options.type
+  if (!dataType) {
+    throw new Error(`Unknown data type for ${options.type}`)
   }
 
-  var dataType = options.subOptions.dataType;
-
-  if ( !dataType ) {
-    dataType = dataTypeForType[options.type];
-    if ( !dataType ) {
-      throw new Error(`Unknown data type for ${options.type}`)
-    }
+  if (!pipeStartByType[options.type]) {
+    throw new Error(`Invalid input type: ${options.type}`)
   }
-
-  if ( !dataTypeMapping[dataType] ) {
+  if (!dataTypeMapping[dataType]) {
     throw new Error(`Unknown data type: ${dataType}`)
   }
+  if (!discriminatorByDataType[dataType]) {
+    throw new Error(`No discriminator for: ${dataType}`)
+  }
 
-  var subOptions = JSON.parse(JSON.stringify(options.subOptions))
+  const subOptions = JSON.parse(JSON.stringify(options.subOptions))
   subOptions.app = options.app
 
-  source(this.pipeline, subOptions)
-  if ( options.logging ) {
-    this.pipeline.push(new log({
-      app: options.app,
-      discriminator: discriminatorByDataType[dataType]
-    }))
-  }
+  const pipeline = [].concat(
+    pipeStartByType[options.type](subOptions),
+    getLogger(options.logging, discriminatorByDataType[dataType]),
+    dataTypeMapping[dataType](subOptions)
+  )
 
-  dataTypeMapping[dataType](this.pipeline, subOptions)
-
-  for (var i = this.pipeline.length - 2; i >= 0; i--) {
-    this.pipeline[i].pipe(this.pipeline[i + 1])
+  for (var i = pipeline.length - 2; i >= 0; i--) {
+    pipeline[i].pipe(pipeline[i + 1])
   }
-  this.pipeline[this.pipeline.length-1].pipe(this)
+  pipeline[pipeline.length - 1].pipe(this)
+
+  this.pipeline = pipeline
 }
 
 require('util').inherits(Simple, Transform)
@@ -67,110 +59,112 @@ Simple.prototype._transform = function (msg, encoding, done) {
 }
 
 Simple.prototype.end = function () {
-  this.pipeline[0].end();
+  this.pipeline[0].end()
 }
 
 module.exports = Simple
 
-const discriminatorByDataType = {
-  'NMEA2000': 'A',
-  'NMEA0183': 'N',
-  'SignalK': 'I'
-}
+const getLogger = (logging, discriminator) =>
+  logging
+    ? [
+      new log({
+        app: options.app,
+        discriminator
+      })
+    ]
+    : []
 
-const pipeStartByType = {
-  'NMEA2000': (pipeline, subOptions) => {
-    var command;
-    var toChildProcess;
-    if ( subOptions.type == 'ngt-1' ) {
-      command = `actisense-serial ${subOptions.device}`,
-      toChildProcess = 'nmea2000out'
-    } else if ( subOptions.type == 'canbus' ) {
-      command = `candump ${subOptions.interface}`
-      toChildProcess = null
-    } else {
-      throw new Error(`unknown NMEA2000 type ${subOptions.type}`)
-    }
-    pipeline.push(new execute({
-      command: command,
-      toChildProcess: toChildProcess,
-      app: subOptions.app
-    }));
-    pipeline.push(new liner(subOptions))
-  },
-  'NMEA0183': (pipeline, subOptions) => {
-    var el
-    if ( subOptions.type == 'tcp' ) {
-      el = new tcp(subOptions);
-    } else if ( subOptions.type === 'udp' ) {
-      el = new udp(subOptions);
-    } else if ( subOptions.type === 'serial' ) {
-      el = new serialport(subOptions);
-    } else {
-      throw new Error(`Unknown networking tyoe: ${options.networking}`)
-    }
-    pipeline.push(el)
-    pipeline.push(new liner(subOptions))
-  },
-  Execute: (pipeline, subOptions) => {
-    pipeline.push(new execute(subOptions));
-    pipeline.push(new liner(subOptions))
-  },
-  "FileStream": (pipeline, subOptions) => {
-    pipeline.push(new filestream(subOptions));
-    if ( subOptions.dataType != 'Multiplexed' ) {
-      pipeline.push(new throttle({
-        rate: subOptions.throttleRate || 1000,
-        app: subOptions.app
-      }));
-    }
-    pipeline.push(new liner(subOptions))
-  },
-  'SignalK': (pipeline, subOptions) => {
-    var el
-    var needsLiner = true
-    if ( subOptions.type === 'ws' || subOptions.type === 'wss' ) {
-      var options = { app: subOptions.app }
-      if ( !subOptions.useDiscovery ) {
-        options.host = subOptions.host
-        options.port = subOptions.port
-      }
-      options.protocol = subOptions.type
-      const mdns_ws = require('./mdns-ws')
-      el = new mdns_ws(options)
-      needsLiner = false
-    } else if ( subOptions.type === 'tcp' ) {
-      el = new tcp(subOptions)
-    } else if ( subOptions.type === 'udp' ) {
-      el = new udp(subOptions)
-    } else {
-      throw new Error(`unknown SignalK type: ${subOptions.type}`)
-    }
-    pipeline.push(el);
-    if ( needsLiner ) {
-      pipeline.push(new liner(subOptions))
-    }
-  }
+const discriminatorByDataType = {
+  NMEA2000: 'A',
+  NMEA0183: 'N',
+  SignalK: 'I'
 }
 
 const dataTypeMapping = {
-  'SignalK': (pipeline, options) => {
-    if ( options.type != 'wss' && options.type != 'ws' ) {
-      pipeline.push(new from_json(options))
+  SignalK: options =>
+    options.type != 'wss' && options.type != 'ws'
+      ? [new from_json(options)]
+      : [],
+  NMEA0183: options => [new nmea0183_signalk(options)],
+  NMEA2000: options => [new n2kAnalyzer(options), new n2k_signalk(options)],
+  Multiplexed: options => [new multiplexedlog(options)]
+}
+
+const pipeStartByType = {
+  NMEA2000: nmea2000input,
+  NMEA0183: nmea0183input,
+  Execute: executeInput,
+  FileStream: fileInput,
+  SignalK: signalKInput
+}
+
+function nmea2000input (subOptions) {
+  let command
+  let toChildProcess
+  if (subOptions.type == 'ngt-1') {
+    command = `actisense-serial ${subOptions.device}`
+    toChildProcess = 'nmea2000out'
+  } else if (subOptions.type == 'canbus') {
+    command = `candump ${subOptions.interface}`
+    toChildProcess = null
+  } else {
+    throw new Error(`unknown NMEA2000 type ${subOptions.type}`)
+  }
+  return [
+    new execute({
+      command: command,
+      toChildProcess: toChildProcess,
+      app: subOptions.app
+    }),
+    new liner(subOptions)
+  ]
+}
+
+function nmea0183input (subOptions) {
+  let el
+  if (subOptions.type == 'tcp') {
+    el = new tcp(subOptions)
+  } else if (subOptions.type === 'udp') {
+    el = new udp(subOptions)
+  } else if (subOptions.type === 'serial') {
+    el = new serialport(subOptions)
+  } else {
+    throw new Error(`Unknown networking tyoe: ${options.networking}`)
+  }
+  return [el, new liner(subOptions)]
+}
+
+function executeInput (subOptions) {
+  return [new execute(subOptions), new liner(subOptions)]
+}
+
+function fileInput (subOptions) {
+  const result = [new filestream(subOptions)]
+  if (subOptions.dataType != 'Multiplexed') {
+    result.push(
+      new throttle({
+        rate: subOptions.throttleRate || 1000,
+        app: subOptions.app
+      })
+    )
+  }
+  result.push(new liner(subOptions))
+  return result
+}
+
+function signalKInput (subOptions) {
+  if (subOptions.type === 'ws' || subOptions.type === 'wss') {
+    const options = { app: subOptions.app }
+    if (!subOptions.useDiscovery) {
+      options.host = subOptions.host
+      options.port = subOptions.port
     }
-  },
-  'NMEA0183': (pipeline, options) => { pipeline.push(new nmea0183_signalk(options)) },
-  'NMEA2000': (pipeline, options) => {
-    pipeline.push(new n2kAnalyzer(options))
-    pipeline.push(new n2k_signalk(options))
-  },
-  'Multiplexed': (pipeline, options) => { pipeline.push(new multiplexedlog(options)) }
+    options.protocol = subOptions.type
+    return [require('./mdns-ws')(options)]
+  } else if (subOptions.type === 'tcp') {
+    return [new tcp(subOptions), new liner(suboptions)]
+  } else if (subOptions.type === 'udp') {
+    return [new udp(subOptions)]
+  }
+  throw new Error(`unknown SignalK type: ${subOptions.type}`)
 }
-
-const dataTypeForType = {
-  'NMEA2000': 'NMEA2000',
-  'NMEA0183': 'NMEA0183',
-  'SignalK': 'SignalK'
-}
-
-                      
