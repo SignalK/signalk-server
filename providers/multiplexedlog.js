cf080e3a948e6a040d0fc6457be6ba3bac8c9bbe
Scott Bender <scott@scottbender.net>
2018-01-28 15:10:22 -0500

    remove: uneeded code

--- providers/multiplexedlog.js
+++ providers/multiplexedlog.js
@@ -1,9 +1,3 @@
 const universal = require('./universal')
 
-function MulitplexLog (options) {
-  universal.call(this, { timestampThrottle: true, noLiner: true, ...options })
-}
-
-require('util').inherits(MulitplexLog, universal)
-
 module.exports = universal
