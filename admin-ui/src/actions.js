d0345832b3a0a9ccd0de1a8be758c8425bab9a3d
Kai Curry <kai@cape.io>
2018-04-15 21:07:12 -0400

    Comment and reference pull request.

--- admin-ui/src/actions.js
+++ admin-ui/src/actions.js
@@ -111,6 +111,7 @@ export function restart () {
   }
 }
 
+// Build actions that perform a basic authFetch to the backend
 export const buildFetchAction = (endpoint, type) => dispatch =>
   authFetch(endpoint)
     .then(response => response.json())
