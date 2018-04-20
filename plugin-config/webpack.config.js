db9473236ba28cdc23e8552abf6074bfa0268f34
Kai Curry <kai@cape.io>
2018-04-20 15:51:53 -0400

    json loader causing issues

--- plugin-config/webpack.config.js
+++ plugin-config/webpack.config.js
@@ -22,10 +22,6 @@ module.exports = {
         query: {
           presets: ['es2015', 'react', 'stage-2']
         }
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
       }
     ]
   },
