6d0620b3c12a83447ce6801a52d8dca0b5a25700
Teppo Kurki <teppo.kurki@iki.fi>
2018-05-04 18:28:56 +0300

    chore: remove unused extra line

--- providers/n2kAnalyzer.js
+++ providers/n2kAnalyzer.js
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- 
+
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -58,7 +58,6 @@ function N2KAnalyzer (options) {
 require('util').inherits(N2KAnalyzer, Transform)
 
 N2KAnalyzer.prototype._transform = function (chunk, encoding, done) {
-  var data = chunk.toString()
   this.analyzerProcess.stdin.write(chunk.toString() + '\n')
   done()
 }
