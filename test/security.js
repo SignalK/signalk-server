e20169e92b8e257534d7d2746cfed94f3ae6a101
Scott Bender <scott@scottbender.net>
2018-09-01 01:48:55 -0400

    feature: provide an API to playback and retrieve historical data (#596)
    This adds new parameters to the ws subscribe request startTime and playbackRate. This will start the stream at the given startTime and play it back at the given rate (2 for example)
    
    wss://localhost:3443/signalk/v1/stream?subscribe=self&startTime=2018-08-24T15:19:09Z&playbackRate=5
    
    Also adds a time parameter to REST requests. This will return the vessel state at the given time.
    
    https://localhost:3443/signalk/v1/api/vessels/self?time=2018-08-24T15:19:09Z

--- test/security.js
+++ test/security.js
@@ -8,6 +8,7 @@ const http = require('http')
 const promisify = require('util').promisify
 const WebSocket = require('ws')
 const _ = require('lodash')
+const { WsPromiser } = require('./servertestutilities')
 
 const limitedSteeringDelta = {
   updates: [
@@ -249,37 +250,3 @@ describe('Security', () => {
     result.status.should.equal(401)
   })
 })
-
-// Connects to the url via ws
-// and provides Promises that are either resolved within
-// timeout period as the next message from the ws or
-// the string "timeout" in case timeout fires
-function WsPromiser (url) {
-  this.ws = new WebSocket(url)
-  this.ws.on('message', this.onMessage.bind(this))
-  this.callees = []
-}
-
-WsPromiser.prototype.nextMsg = function () {
-  const callees = this.callees
-  return new Promise((resolve, reject) => {
-    callees.push(resolve)
-    setTimeout(_ => {
-      resolve('timeout')
-    }, 250)
-  })
-}
-
-WsPromiser.prototype.onMessage = function (message) {
-  const theCallees = this.callees
-  this.callees = []
-  theCallees.forEach(callee => callee(message))
-}
-
-WsPromiser.prototype.send = function (message) {
-  const that = this
-  return new Promise((resolve, reject) => {
-    that.ws.send(JSON.stringify(message))
-    setTimeout(() => resolve('wait over'), 100)
-  })
-}
--- test/subscriptions.js
+++ test/subscriptions.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const freeport = require('freeport-promise')
 const WebSocket = require('ws')
 const rp = require('request-promise')
-const startServerP = require('./servertestutilities').startServerP
+const { startServerP, WsPromiser } = require('./servertestutilities')
 
 function getDelta (overwrite) {
   const delta = {
@@ -484,37 +484,3 @@ describe('Subscriptions', _ => {
       })
   })
 })
-
-// Connects to the url via ws
-// and provides Promises that are either resolved within
-// timeout period as the next message from the ws or
-// the string "timeout" in case timeout fires
-function WsPromiser (url) {
-  this.ws = new WebSocket(url)
-  this.ws.on('message', this.onMessage.bind(this))
-  this.callees = []
-}
-
-WsPromiser.prototype.nextMsg = function () {
-  const callees = this.callees
-  return new Promise((resolve, reject) => {
-    callees.push(resolve)
-    setTimeout(_ => {
-      resolve('timeout')
-    }, 250)
-  })
-}
-
-WsPromiser.prototype.onMessage = function (message) {
-  const theCallees = this.callees
-  this.callees = []
-  theCallees.forEach(callee => callee(message))
-}
-
-WsPromiser.prototype.send = function (message) {
-  const that = this
-  return new Promise((resolve, reject) => {
-    that.ws.send(JSON.stringify(message))
-    setTimeout(() => resolve('wait over'), 100)
-  })
-}
