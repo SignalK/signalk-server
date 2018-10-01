neftaly/signalk-server-node

feature: provide a way for devices to obtain a security token and new user to sign up

packages/server-admin-ui/src/views/security/AccessRequests.js
@@ -1,0 +1,189 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Form,
+  Col,
+  Label,
+  FormGroup,
+  FormText,
+  Table,
+  Row
+} from 'reactstrap';
+import EnableSecurity from './EnableSecurity'
+    
+class AccessRequests extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRequest: null,
+      accessRequestsApproving: [],
+      accessRequestsDenying: []
+    }
+    this.handleRequestChange = this.handleRequestChange.bind(this)
+  }
+
+  handleAccessRequest(identifier, approved) {
+    var stateKey = approved ? 'accessRequestsApproving' : 'accessRequestsDenying'
+    this.state[stateKey].push(identifier)
+    this.setState({stateKey: this.state})
+
+    var payload = {
+      permissions: this.state.selectedRequest.permissions || 'readonly',
+      config: this.state.selectedRequest.config,
+      expiration: this.state.selectedRequest.expiration || '1y'
+    }
+    
+    fetch(`/security/access/requests/${identifier}/${approved ? 'approved' : 'denied'}`, {
+      method: 'PUT',
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
+    }).then(response => response.text())
+      .then(response => {
+        this.state[stateKey] = this.state[stateKey].filter(id => id != identifier)
+        this.setState({
+          stateKey: this.state[stateKey],
+          selectedRequest: null
+        })
+      });
+  }
+
+  requestClicked(event, request, index) {
+    this.setState({
+      selectedRequest: JSON.parse(JSON.stringify(request)),
+      selectedIndex: index
+    }, () => {
+      this.refs['selectedRequest'].scrollIntoView();
+    });
+  }
+
+    handleRequestChange(event) {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    this.state.selectedRequest[event.target.name] = value;
+    this.setState({
+      selectedRequest: this.state.selectedRequest
+    });
+  }
+  handleCancel(event) {
+    this.setState({ selectedRequest: null })
+  }
+
+  
+  render () {
+    return (
+      <div className='animated fadeIn'>
+      {this.props.loginStatus.authenticationRequired === false &&
+       <EnableSecurity />
+      }
+      {this.props.loginStatus.authenticationRequired &&
+       <div>
+       <Card>
+       <CardHeader>
+          <i className="fa fa-align-justify"></i>Access Requests
+        </CardHeader>
+        <CardBody>
+          <Table hover responsive bordered striped size="sm">
+            <thead>
+                <tr>
+                <th>Identifier</th>
+                <th>Description</th>
+                <th>Source IP</th>
+              </tr>
+            </thead>
+            <tbody>
+            {(this.props.accessRequests || []).map((req, index) => {
+            return (
+                <tr key={req.identifier} onClick={this.requestClicked.bind(this, event, req, index)}>
+                <td>{req.identifier}</td>
+                <td>{req.description}</td>
+                <td>{req.ip}</td>
+                </tr>
+            )
+            })}
+            </tbody>
+          </Table>
+        </CardBody>
+             </Card>
+
+      {this.state.selectedRequest &&
+      <div ref='selectedRequest'>
+      <Card>
+        <CardHeader>
+          <i className="fa fa-align-justify"></i>Request
+        </CardHeader>
+        <CardBody>
+          <FormGroup row>
+            <Col md="2">
+              <Label>Identifier</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Label>{this.state.selectedRequest.identifier}</Label>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label>Description</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Label>{this.state.selectedRequest.description}</Label>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label htmlFor="text-input">Authentication Timeout</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Input type="text" name="expiration" onChange={this.handleRequestChange} value={this.state.selectedRequest.expiration} />
+               <FormText color="muted">Exmaples: 60s, 1m, 1h, 1d, NEVER</FormText>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label htmlFor="select">Permissions</Label>
+            </Col>
+            <Col xs="12" md="2">
+              <Input type="select" name="permissions" value={this.state.selectedRequest.permissions} onChange={this.handleRequestChange}>
+                <option value="readonly">Read Only</option>
+                <option value="readwrite">Read/Write</option>
+                <option value="admin">Admin</option>
+              </Input>
+            </Col>
+          </FormGroup>
+        </CardBody>
+        <CardFooter>
+          <Row>
+            <Col xs="4" md="1">
+              <Button size="sm" color="danger" onClick={this.handleAccessRequest.bind(this, this.state.selectedRequest.identifier, true)}><i className={this.state.accessRequestsApproving.indexOf(this.state.selectedRequest.identifier) != -1 ? "fa fa-spinner fa-spin" : "fa fa-plus-circle"}></i> Approve</Button>
+ 
+            </Col>
+            <Col xs="4" md="1">
+               <Button size="sm" color="primary" onClick={this.handleAccessRequest.bind(this, this.state.selectedRequest.identifier, false)}><i className={this.state.accessRequestsDenying.indexOf(this.state.selectedRequest.identifier) != -1 ? "fa fa-spinner fa-spin" : "fa fa-ban"}></i> Deny</Button>
+            </Col>
+          </Row>
+        </CardFooter>
+      </Card>
+        </div>
+      }             
+             
+       </div>
+      }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ accessRequests, loginStatus  }) => ({ accessRequests, loginStatus })
+
+export default connect(mapStateToProps)(AccessRequests)
+
+

packages/server-admin-ui/src/views/security/Register.js
@@ -1,0 +1,114 @@
+import React, {Component} from 'react';
+import {Container, Row, Col, Card, CardBody, CardFooter, Button, Input, InputGroup, InputGroupAddon, FormText} from 'reactstrap';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: null,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      registrationSent: false
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+  }
+
+  handleInputChange(event) {
+    var targetName = event.target.name
+    this.setState({[event.target.name]: event.target.value}, () => {
+      if ( targetName === 'password' ||
+           targetName === 'confirmPassword' &&
+           this.state.password != this.state.confirmPassword ) {
+        this.setState({errorMessage: "Passwords do not match"})
+      } else {
+        this.setState({errorMessage: null})
+      }
+    })
+  }
+
+  handleCreate(event) {
+    if ( this.state.email.length == 0 ) {
+      this.setState({errorMessage: 'Please enter an email address'})
+    } else if ( this.state.password.length == 0 &&
+                this.state.confirmPassword.length == 0 ) {
+      this.setState({errorMessage: 'Please enter and conform your password'})
+    } else if ( this.state.password != this.state.confirmPassword ) {
+      //error message is already thwre
+      return
+    } else {
+      var payload = {
+        userId: this.state.email,
+        password: this.state.password
+      }
+      fetch(`/access/requests`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+        credentials: "include"
+      })
+        .then(response => {
+          if ( response.status != 200 ) {
+            response.text().then(text => {
+              this.setState({errorMessage: text})
+            })
+          } else {
+            this.setState({registrationSent: true})
+          }
+        });
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="6">
+              <Card className="mx-4">
+              <CardBody className="p-4">
+                  <h1>Register</h1>
+                  {this.state.registrationSent &&
+                   <p className="text-muted">Your registration has been sent</p>
+                  }
+                  {!this.state.registrationSent &&
+                   <div>
+                   <p className="text-muted">Create your account</p>
+                  <InputGroup className="mb-3">
+                    <InputGroupAddon>@</InputGroupAddon>
+                    <Input name="email" type="text" placeholder="Email" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
+                    <Input name="password" type="password" placeholder="Password" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <InputGroup className="mb-4">
+                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
+        <Input name="confirmPassword" type="password" placeholder="Repeat password" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <p className="text-danger">{this.state.errorMessage}</p>
+                  </div>
+                  }
+                </CardBody>
+                {!this.state.registrationSent &&
+                <CardFooter className="p-4">
+                  <Row>
+                    <Col xs="12" sm="12">
+                      <Button color="success" block onClick={this.handleCreate}>Create Account</Button>
+                    </Col>
+                  </Row>
+                 </CardFooter>
+                }
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Register;

SignalK/signalk-server

packages/server-admin-ui/src/views/security/AccessRequests.js
@@ -1,0 +1,206 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Form,
+  Col,
+  Label,
+  FormGroup,
+  FormText,
+  Table,
+  Row
+} from 'reactstrap';
+import EnableSecurity from './EnableSecurity'
+    
+class AccessRequests extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRequest: null,
+      accessRequestsApproving: [],
+      accessRequestsDenying: []
+    }
+    this.handleRequestChange = this.handleRequestChange.bind(this)
+  }
+
+  handleAccessRequest(identifier, approved) {
+    var stateKey = approved ? 'accessRequestsApproving' : 'accessRequestsDenying'
+    this.state[stateKey].push(identifier)
+    this.setState({stateKey: this.state})
+
+    var payload = {
+      permissions: this.state.selectedRequest.permissions || 'readonly',
+      config: this.state.selectedRequest.config,
+      expiration: this.state.selectedRequest.expiration || '1y'
+    }
+    
+    fetch(`/security/access/requests/${identifier}/${approved ? 'approved' : 'denied'}`, {
+      method: 'PUT',
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
+    }).then(response => response.text())
+      .then(response => {
+        this.state[stateKey] = this.state[stateKey].filter(id => id != identifier)
+        this.setState({
+          stateKey: this.state[stateKey],
+          selectedRequest: null
+        })
+      });
+  }
+
+  requestClicked(event, request, index) {
+    this.setState({
+      selectedRequest: JSON.parse(JSON.stringify(request)),
+      selectedIndex: index
+    }, () => {
+      this.refs['selectedRequest'].scrollIntoView();
+    });
+  }
+
+    handleRequestChange(event) {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    this.state.selectedRequest[event.target.name] = value;
+    this.setState({
+      selectedRequest: this.state.selectedRequest
+    });
+  }
+  handleCancel(event) {
+    this.setState({ selectedRequest: null })
+  }
+
+  
+  render () {
+    return (
+      <div className='animated fadeIn'>
+      {this.props.loginStatus.authenticationRequired === false &&
+       <EnableSecurity />
+      }
+      {this.props.loginStatus.authenticationRequired &&
+       <div>
+       <Card>
+       <CardHeader>
+          <i className="fa fa-align-justify"></i>Access Requests
+        </CardHeader>
+        <CardBody>
+          <Table hover responsive bordered striped size="sm">
+            <thead>
+                <tr>
+                <th>Identifier</th>
+                <th>Description</th>
+                <th>Source IP</th>
+              </tr>
+            </thead>
+            <tbody>
+            {(this.props.accessRequests || []).map((req, index) => {
+            return (
+                <tr key={req.accessIdentifier} onClick={this.requestClicked.bind(this, event, req, index)}>
+                <td>{req.accessIdentifier}</td>
+                <td>{req.accessDescription}</td>
+                <td>{req.ip}</td>
+                </tr>
+            )
+            })}
+            </tbody>
+          </Table>
+        </CardBody>
+             </Card>
+
+      {this.state.selectedRequest &&
+      <div ref='selectedRequest'>
+      <Card>
+        <CardHeader>
+          <i className="fa fa-align-justify"></i>Request
+        </CardHeader>
+        <CardBody>
+          <FormGroup row>
+            <Col md="2">
+              <Label>Identifier</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Label>{this.state.selectedRequest.accessIdentifier}</Label>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label>Description</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Label>{this.state.selectedRequest.accessDescription}</Label>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label htmlFor="text-input">Authentication Timeout</Label>
+            </Col>
+            <Col xs="12" md="9">
+              <Input type="text" name="expiration" onChange={this.handleRequestChange} value={this.state.selectedRequest.expiration} />
+               <FormText color="muted">Exmaples: 60s, 1m, 1h, 1d, NEVER</FormText>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col md="2">
+              <Label htmlFor="select">Permissions</Label>
+            </Col>
+           <Col xs="12" md="2">
+             {!this.state.selectedRequest.requestedPermissions && (
+              <Input type="select" name="permissions" value={this.state.selectedRequest.permissions} onChange={this.handleRequestChange}>
+                <option value="readonly">Read Only</option>
+                <option value="readwrite">Read/Write</option>
+                <option value="admin">Admin</option>
+                 </Input>
+             )}
+             {this.state.selectedRequest.requestedPermissions && (
+              <Label>{convertPermissions(this.state.selectedRequest.permissions)}</Label>
+              )}
+            </Col>
+          </FormGroup>
+        </CardBody>
+        <CardFooter>
+          <Row>
+            <Col xs="4" md="1">
+              <Button size="sm" color="danger" onClick={this.handleAccessRequest.bind(this, this.state.selectedRequest.accessIdentifier, true)}><i className={this.state.accessRequestsApproving.indexOf(this.state.selectedRequest.accessIdentifier) != -1 ? "fa fa-spinner fa-spin" : "fa fa-plus-circle"}></i> Approve</Button>
+ 
+            </Col>
+            <Col xs="4" md="1">
+               <Button size="sm" color="primary" onClick={this.handleAccessRequest.bind(this, this.state.selectedRequest.accessIdentifier, false)}><i className={this.state.accessRequestsDenying.indexOf(this.state.selectedRequest.accessIdentifier) != -1 ? "fa fa-spinner fa-spin" : "fa fa-ban"}></i> Deny</Button>
+            </Col>
+          </Row>
+        </CardFooter>
+      </Card>
+        </div>
+      }             
+             
+       </div>
+      }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ accessRequests, loginStatus  }) => ({ accessRequests, loginStatus })
+
+export default connect(mapStateToProps)(AccessRequests)
+
+function convertPermissions (type) {
+  if (type == 'readonly') {
+    return 'Read Only'
+  } else if (type == 'readwrite') {
+    return 'Read/Write'
+  } else if (type == 'admin') {
+    return 'Admin'
+  } else {
+    return `Unknown ${type}`
+  }
+}
+
+

packages/server-admin-ui/src/views/security/Register.js
@@ -1,0 +1,114 @@
+import React, {Component} from 'react';
+import {Container, Row, Col, Card, CardBody, CardFooter, Button, Input, InputGroup, InputGroupAddon, FormText} from 'reactstrap';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: null,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      registrationSent: false
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+  }
+
+  handleInputChange(event) {
+    var targetName = event.target.name
+    this.setState({[event.target.name]: event.target.value}, () => {
+      if ( targetName === 'password' ||
+           targetName === 'confirmPassword' &&
+           this.state.password != this.state.confirmPassword ) {
+        this.setState({errorMessage: "Passwords do not match"})
+      } else {
+        this.setState({errorMessage: null})
+      }
+    })
+  }
+
+  handleCreate(event) {
+    if ( this.state.email.length == 0 ) {
+      this.setState({errorMessage: 'Please enter an email address'})
+    } else if ( this.state.password.length == 0 &&
+                this.state.confirmPassword.length == 0 ) {
+      this.setState({errorMessage: 'Please enter and conform your password'})
+    } else if ( this.state.password != this.state.confirmPassword ) {
+      //error message is already thwre
+      return
+    } else {
+      var payload = {
+        userId: this.state.email,
+        password: this.state.password
+      }
+      fetch(`/signalk/v1/access/requests`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+        credentials: "include"
+      })
+        .then(response => {
+          if ( response.status != 202 ) {
+            response.json().then(json => {
+              this.setState({errorMessage: json.message ? json.message : json.result})
+            })
+          } else {
+            this.setState({registrationSent: true})
+          }
+        });
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="6">
+              <Card className="mx-4">
+              <CardBody className="p-4">
+                  <h1>Register</h1>
+                  {this.state.registrationSent &&
+                   <p className="text-muted">Your registration has been sent</p>
+                  }
+                  {!this.state.registrationSent &&
+                   <div>
+                   <p className="text-muted">Create your account</p>
+                  <InputGroup className="mb-3">
+                    <InputGroupAddon>@</InputGroupAddon>
+                    <Input name="email" type="text" placeholder="Email" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
+                    <Input name="password" type="password" placeholder="Password" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <InputGroup className="mb-4">
+                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
+        <Input name="confirmPassword" type="password" placeholder="Repeat password" onChange={this.handleInputChange}/>
+                  </InputGroup>
+                  <p className="text-danger">{this.state.errorMessage}</p>
+                  </div>
+                  }
+                </CardBody>
+                {!this.state.registrationSent &&
+                <CardFooter className="p-4">
+                  <Row>
+                    <Col xs="12" sm="12">
+                      <Button color="success" block onClick={this.handleCreate}>Create Account</Button>
+                    </Col>
+                  </Row>
+                 </CardFooter>
+                }
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Register;