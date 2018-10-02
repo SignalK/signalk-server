8b350fc861f1cdbf938a54dd19477e65d721edae
Scott Bender <scott@scottbender.net>
2018-10-01 21:29:44 -0400

    tests: add put tests

---
+++ test/put.js
@@ -0,0 +1,232 @@
+const chai = require('chai')
+chai.Should()
+chai.use(require('chai-things'))
+const freeport = require('freeport-promise')
+const Server = require('../lib')
+const fetch = require('node-fetch')
+const { registerActionHandler } = require('../lib/put')
+const WebSocket = require('ws')
+const { WsPromiser } = require('./servertestutilities')
+
+const sleep = ms => new Promise(res => setTimeout(res, ms))
+
+describe('Put Requests', () => {
+  var server, url, port
+
+  before(async function () {
+    port = await freeport()
+    url = `http://0.0.0.0:${port}`
+    const serverApp = new Server({
+      config: {
+        settings: {
+          port,
+          interfaces: {
+            plugins: false
+          }
+        }
+      }
+    })
+    server = await serverApp.start()
+
+    function switch2Handler (context, path, value, cb) {
+      if (typeof value !== 'number') {
+        return { state: 'COMPLETED', result: 400, message: 'invalid value' }
+      } else {
+        setTimeout(() => {
+          server.app.handleMessage('test', {
+            updates: [
+              {
+                values: [
+                  { path: 'electrical.switches.switch2.state', value: value }
+                ]
+              }
+            ]
+          })
+          cb({ state: 'COMPLETED', result: 200 })
+        }, 100)
+        return { state: 'PENDING' }
+      }
+    }
+
+    registerActionHandler(
+      'vessels.self',
+      'electrical.switches.switch2.state',
+      null,
+      switch2Handler
+    )
+  })
+
+  it('HTTP put to unhandled path fails', async function () {
+    var result = await fetch(
+      `${url}/signalk/v1/api/vessels/self/electrical/switches/switch1.state`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          value: 1
+        })
+      }
+    )
+
+    result.status.should.equal(405)
+  })
+
+  it('HTTP successfull put', async function () {
+    var result = await fetch(
+      `${url}/signalk/v1/api/vessels/self/electrical/switches/switch2.state`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          value: 1
+        })
+      }
+    )
+
+    result.status.should.equal(202)
+
+    var json = await result.json()
+    json.should.have.property('state')
+    json.state.should.equal('PENDING')
+    json.should.have.property('href')
+
+    await sleep(200)
+
+    var result = await fetch(`${url}${json.href}`)
+
+    result.status.should.equal(200)
+
+    var json = await result.json()
+    json.should.have.property('state')
+    json.state.should.equal('COMPLETED')
+  })
+
+  it('HTTP failing put', async function () {
+    var result = await fetch(
+      `${url}/signalk/v1/api/vessels/self/electrical/switches/switch2/state`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          value: 'dummy'
+        })
+      }
+    )
+
+    result.status.should.equal(400)
+
+    var json = await result.json()
+    json.should.have.property('state')
+    json.state.should.equal('COMPLETED')
+    json.should.have.property('message')
+    json.message.should.equal('invalid value')
+  })
+
+  it('WS put to unhandled path fails', async function () {
+    var ws = new WsPromiser(
+      `ws://0.0.0.0:${port}/signalk/v1/stream?subsribe=none`
+    )
+    var msg = await ws.nextMsg()
+
+    let readPromise = ws.nextMsg()
+
+    let something = await ws.send({
+      context: 'vessels.self',
+      put: {
+        path: 'electrical.switches.switch1.state',
+        value: 1
+      }
+    })
+
+    msg = await readPromise
+    msg.should.not.equal('timeout')
+    let response = JSON.parse(msg)
+    response.should.have.property('result')
+    response.result.should.equal(405)
+  })
+
+  it('WS successfull put', async function () {
+    var ws = new WsPromiser(
+      `ws://0.0.0.0:${port}/signalk/v1/stream?subsribe=none`
+    )
+    var msg = await ws.nextMsg()
+
+    let readPromise = ws.nextMsg()
+
+    let something = await ws.send({
+      context: 'vessels.self',
+      put: {
+        path: 'electrical.switches.switch2.state',
+        value: 1
+      }
+    })
+
+    msg = await readPromise
+    msg.should.not.equal('timeout')
+    let response = JSON.parse(msg)
+    response.should.have.property('state')
+    response.state.should.equal('PENDING')
+    response.should.have.property('href')
+
+    let resultPromise = ws.nextMsg()
+    let updatePromise = ws.nextMsg()
+
+    response = await resultPromise
+    console.log(response)
+
+    let update = await updatePromise
+    console.log(`HELLO ${update}`)
+
+    /*
+    await sleep(200)
+
+    var result = await fetch(`${url}${json.href}`)
+
+    result.status.should.equal(200)
+
+    var json = await result.json()
+    json.should.have.property('state')
+    json.state.should.equal('COMPLETED')
+    */
+  })
+  /*
+  it('HTTP failing put', async function() {
+    var readPromiser = new WsPromiser(
+      `ws://0.0.0.0:${port}/signalk/v1/stream?subsribe=none`
+    )
+    var msg = await readPromiser.nextMsg()
+
+    await readPromiser.send({
+      context: 'vessels.self',
+      put: {
+        path: 'electrical.switches.switch2.state',
+        value: 'dummy'
+      }
+    })
+
+    var result = await fetch(`${url}/signalk/v1/api/vessels/self/electrical/switches/switch2/state`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value: 'dummy'
+      })
+    })
+
+    result.status.should.equal(400)
+
+    var json = await result.json()
+    json.should.have.property('state')
+    json.state.should.equal('COMPLETED')
+    json.should.have.property('message')
+    json.message.should.equal('invalid value')
+  })
+  */
+})
