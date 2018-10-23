43e4144282025e285428d645679be551d0a3a09e
Scott Bender <scott@scottbender.net>
2018-10-23 11:56:14 -0400

    tests: add tests for application data

--- test/security.js
+++ test/security.js
@@ -2,13 +2,16 @@ const chai = require('chai')
 chai.Should()
 chai.use(require('chai-things'))
 const freeport = require('freeport-promise')
-const Server = require('../lib')
 const fetch = require('node-fetch')
 const http = require('http')
-const promisify = require('util').promisify
 const WebSocket = require('ws')
 const _ = require('lodash')
-const { WsPromiser } = require('./servertestutilities')
+const {
+  WsPromiser,
+  startServerP,
+  getReadOnlyToken,
+  getWriteToken
+} = require('./servertestutilities')
 
 const limitedSteeringDelta = {
   updates: [
@@ -44,20 +47,13 @@ const openNavigationDelta = {
 }
 
 const WRITE_USER_NAME = 'writeuser'
-const WRITE_USER_PASSWORD = 'writepass'
 const LIMITED_USER_NAME = 'testuser'
-const LIMITED_USER_PASSWORD = 'verylimited'
 
 describe('Security', () => {
   var server, url, port, readToken, writeToken
 
   before(async function () {
     var securityConfig = {
-      allow_readonly: false,
-      expiration: '1d',
-      secretKey:
-        '3ad6c2b567c43199e1afd2307ef506ea9fb5f8becada1f86c15213d75124fbaf4647c3f7202b788bba5c01c8bb8fdc52e8ca5bd484be36b6900ac03b88b6063b6157bee1e638acde1936d6ef4717884de63c86e9f50c8ee12b15bf837268b04bc09a461f5dddaf71dfc7205cc549b29810a31515b21d57ac5fdde29628ccff821cfc229004c4864576eb7c238b0cd3a6d774c14854affa1aeedbdb1f47194033f18e50d9dc1171a47e36f26c864080a627c500d1642fc94f71e93ff54022a8d4b00f19e88a0610ef70708ac6a386ba0df7cab201e24d3eb0061ddd0052d3d85cda50ac8d6cafc4ecc43d8db359a85af70d4c977a3d4b0d588f123406dbd57f01',
-      users: [],
       acls: [
         {
           context: 'vessels.self',
@@ -89,63 +85,19 @@ describe('Security', () => {
       ]
     }
 
-    port = await freeport()
-    url = `http://0.0.0.0:${port}`
-    const serverApp = new Server(
-      {
-        config: {
-          settings: {
-            port,
-            interfaces: {
-              plugins: false
-            },
-            security: {
-              strategy: './tokensecurity'
-            }
-          }
-        }
-      },
-      securityConfig
-    )
-    server = await serverApp.start()
+    server = await freeport().then(p => {
+      port = p
+      url = `http://0.0.0.0:${port}`
+      return startServerP(p, true, securityConfig)
+    })
 
-    await promisify(server.app.securityStrategy.addUser)(securityConfig, {
-      userId: LIMITED_USER_NAME,
-      type: 'readwrite',
-      password: LIMITED_USER_PASSWORD
-    })
-    await promisify(server.app.securityStrategy.addUser)(securityConfig, {
-      userId: WRITE_USER_NAME,
-      type: 'readwrite',
-      password: WRITE_USER_PASSWORD
-    })
-    readToken = await login(LIMITED_USER_NAME, LIMITED_USER_PASSWORD)
-    writeToken = await login(WRITE_USER_NAME, WRITE_USER_PASSWORD)
+    readToken = await getReadOnlyToken(server)
+    writeToken = await getWriteToken(server)
   })
 
   after(async function () {
     await server.stop()
   })
-
-  async function login (username, password) {
-    const result = await fetch(`${url}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username,
-        password
-      })
-    })
-    if (result.status != 200) {
-      throw new Error('Login returned ' + result.status)
-    }
-    return result.json().then(json => {
-      console.log(json)
-      return json.token
-    })
-  }
 
   it('unathorized request fails', async function () {
     var result = await fetch(`${url}/signalk/v1/api/vessels/self`)
@@ -159,7 +111,7 @@ describe('Security', () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        username: 'LIMITED_USER_NAME',
+        username: LIMITED_USER_NAME,
         password: 'badpassword'
       })
     })
@@ -167,12 +119,9 @@ describe('Security', () => {
   })
 
   it('login works', async function () {
-    const writeUserToken = await login(WRITE_USER_NAME, WRITE_USER_PASSWORD)
+    const writeUserToken = await getWriteToken(server)
     writeUserToken.length.should.equal(151)
-    const limitedUserToken = await login(
-      LIMITED_USER_NAME,
-      LIMITED_USER_PASSWORD
-    )
+    const limitedUserToken = await getReadOnlyToken(server)
     limitedUserToken.length.should.equal(149)
   })
 
--- test/servertestutilities.js
+++ test/servertestutilities.js
@@ -1,4 +1,6 @@
 const WebSocket = require('ws')
+const promisify = require('util').promisify
+const fetch = require('node-fetch')
 
 // Connects to the url via ws
 // and provides Promises that are either resolved within
@@ -34,11 +36,27 @@ WsPromiser.prototype.send = function (message) {
   })
 }
 
+const defaultSecurityConfig = {
+  allow_readonly: false,
+  expiration: '1d',
+  secretKey:
+    '3ad6c2b567c43199e1afd2307ef506ea9fb5f8becada1f86c15213d75124fbaf4647c3f7202b788bba5c01c8bb8fdc52e8ca5bd484be36b6900ac03b88b6063b6157bee1e638acde1936d6ef4717884de63c86e9f50c8ee12b15bf837268b04bc09a461f5dddaf71dfc7205cc549b29810a31515b21d57ac5fdde29628ccff821cfc229004c4864576eb7c238b0cd3a6d774c14854affa1aeedbdb1f47194033f18e50d9dc1171a47e36f26c864080a627c500d1642fc94f71e93ff54022a8d4b00f19e88a0610ef70708ac6a386ba0df7cab201e24d3eb0061ddd0052d3d85cda50ac8d6cafc4ecc43d8db359a85af70d4c977a3d4b0d588f123406dbd57f01',
+  users: []
+}
+
+const WRITE_USER_NAME = 'writeuser'
+const WRITE_USER_PASSWORD = 'writepass'
+const LIMITED_USER_NAME = 'testuser'
+const LIMITED_USER_PASSWORD = 'verylimited'
+const ADMIN_USER_NAME = 'adminuser'
+const ADMIN_USER_PASSWORD = 'admin'
+
 module.exports = {
   WsPromiser: WsPromiser,
-  startServerP: function startServerP (port) {
+  startServerP: function startServerP (port, enableSecurity, securityConfig) {
     const Server = require('../lib')
-    const server = new Server({
+
+    const props = {
       config: {
         defaults: {
           vessels: {
@@ -64,7 +82,83 @@ module.exports = {
           }
         }
       }
+    }
+
+    if (enableSecurity) {
+      props.config.settings.security = {
+        strategy: './tokensecurity'
+      }
+    }
+
+    process.env.SIGNALK_NODE_CONDFIG_DIR = require('path').join(
+      __dirname,
+      'server-test-config'
+    )
+
+    props.securityConfig = {
+      ...defaultSecurityConfig,
+      ...(securityConfig || {})
+    }
+    const server = new Server(props)
+    return new Promise((resolve, reject) => {
+      server.start().then(s => {
+        Promise.all([
+          promisify(s.app.securityStrategy.addUser)(props.securityConfig, {
+            userId: LIMITED_USER_NAME,
+            type: 'read',
+            password: LIMITED_USER_PASSWORD
+          }),
+          promisify(s.app.securityStrategy.addUser)(props.securityConfig, {
+            userId: WRITE_USER_NAME,
+            type: 'readwrite',
+            password: WRITE_USER_PASSWORD
+          }),
+          promisify(s.app.securityStrategy.addUser)(props.securityConfig, {
+            userId: ADMIN_USER_NAME,
+            type: 'admin',
+            password: ADMIN_USER_PASSWORD
+          })
+        ])
+          .then(() => {
+            resolve(s)
+          })
+          .catch(reject)
+      })
     })
-    return server.start()
+  },
+  getReadOnlyToken: server => {
+    return login(server, LIMITED_USER_NAME, LIMITED_USER_PASSWORD)
+  },
+  getWriteToken: server => {
+    return login(server, WRITE_USER_NAME, WRITE_USER_PASSWORD)
+  },
+  getAdminToken: server => {
+    return login(server, ADMIN_USER_NAME, ADMIN_USER_PASSWORD)
   }
+}
+
+function login (server, username, password) {
+  return new Promise((resolve, reject) => {
+    fetch(`http://0.0.0.0:${server.app.config.settings.port}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+      .then(result => {
+        if (result.status != 200) {
+          result.text().then(t => {
+            reject(new Error(`Login returned ${result.status}: ${t}`))
+          })
+        }
+        result.json().then(json => {
+          resolve(json.token)
+        })
+      })
+      .catch(reject)
+  })
 }
