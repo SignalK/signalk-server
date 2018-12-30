SignalK/signalk-server

commit a732e2eba8f9e319a056b2780d9a47eaf9bda6b2

feature: show the status of providers and plugins on the dashboard (#591)

providers/serialport.js
@@ -85,15 +85,32 @@ SerialStream.prototype.start = function () {
   this.serial.on(
     'open',
     function () {
+      this.options.app.setProviderStatus(
+        this.options.providerId,
+        `Connected to ${this.options.device}`
+      )
       const parser = new SerialPort.parsers.Readline()
       this.serial.pipe(parser).pipe(this)
     }.bind(this)
   )
 
-  this.serial.on('error', function (x) {
-    console.log(x)
-  })
-  this.serial.on('close', this.start.bind(this))
+  this.serial.on(
+    'error',
+    function (x) {
+      this.options.app.setProviderError(this.options.providerId, x.message)
+      console.log(x)
+    }.bind(this)
+  )
+  this.serial.on(
+    'close',
+    function () {
+      this.options.app.setProviderError(
+        this.options.providerId,
+        'Closed, reconnecting...'
+      )
+      this.start.bind(this)
+    }.bind(this)
+  )
 
   var that = this
   let pendingWrites = 0

lib/index.js
@@ -62,6 +62,66 @@ function Server(opts) {
   const deltachain = new DeltaChain(app.signalk.addDelta.bind(app.signalk))
   app.registerDeltaInputHandler = deltachain.register
 
+  app.providerStatus = {}
+
+  app.setProviderStatus = function(providerId, statusMessage) {
+    doSetProviderStatus(providerId, statusMessage, 'status')
+  }
+
+  app.setProviderError = function(providerId, errorMessage) {
+    doSetProviderStatus(providerId, errorMessage, 'error')
+  }
+
+  function doSetProviderStatus(providerId, statusMessage, type) {
+    if (_.isUndefined(app.providerStatus[providerId])) {
+      app.providerStatus[providerId] = {}
+    }
+    const status = app.providerStatus[providerId]
+
+    status.type = type
+    status.id = providerId
+    status.timeStamp = new Date().toLocaleString()
+
+    if (type === 'error' && status.message != statusMessage) {
+      status.lastError = statusMessage
+      status.lastErrorTimeStamp = status.timeStamp
+    }
+
+    status.message = statusMessage
+
+    app.emit('serverevent', {
+      type: 'PROVIDERSTATUS',
+      from: 'signalk-server',
+      data: app.getProviderStatus()
+    })
+  }
+
+  app.getProviderStatus = function() {
+    let providerStatus = _.values(app.providerStatus)
+
+    if (app.plugins) {
+      app.plugins.forEach(plugin => {
+        if (
+          typeof plugin.statusMessage === 'function' &&
+          _.isUndefined(app.providerStatus[plugin.name])
+        ) {
+          let message = plugin.statusMessage()
+          if (message) {
+            message = message.trim()
+            if (message.length > 0) {
+              providerStatus.push({
+                message: message,
+                type: 'status',
+                id: plugin.id
+              })
+            }
+          }
+        }
+      })
+    }
+    return providerStatus
+  }
+
   app.handleMessage = function(providerId, data) {
     if (data && data.updates) {
       incDeltaStatistics(app, providerId)
@@ -112,6 +172,15 @@ Server.prototype.start = function() {
       app.deltaCache.pruneContexts.bind(app.deltaCache, 60 * 60),
       60 * 1000
     )
+  )
+  app.intervals.push(
+    setInterval(() => {
+      app.emit('serverevent', {
+        type: 'PROVIDERSTATUS',
+        from: 'signalk-server',
+        data: app.getProviderStatus()
+      })
+    }, 5 * 1000)
   )
 
   this.app.providers = []

lib/interfaces/plugins.js
@@ -44,6 +44,7 @@ module.exports = function (app) {
       router = express.Router()
 
       app.get('/plugins', function (req, res, next) {
+        let providerStatus = app.getProviderStatus()
         res.json(
           _.sortBy(app.plugins, [
             plugin => {
@@ -60,10 +61,8 @@ module.exports = function (app) {
               typeof plugin.schema === 'function'
                 ? plugin.schema()
                 : plugin.schema
-            const statusMessage =
-              typeof plugin.statusMessage === 'function'
-                ? plugin.statusMessage()
-                : ''
+            const status = providerStatus.find(p => p.id == plugin.name)
+            const statusMessage = status ? status.message : ''
             const uiSchema =
               typeof plugin.uiSchema === 'function'
                 ? plugin.uiSchema()
@@ -206,24 +205,64 @@ module.exports = function (app) {
     })
     onStopHandlers[plugin.id] = []
     plugin.stop()
+    app.setProviderStatus(plugin.name, 'Stopped')
     debug('Stopped plugin ' + plugin.name)
   }
 
-  function doRegisterPlugin (app, pluginName, metadata, location) {
+  function setPluginStartedMessage (plugin) {
+    let statusMessage =
+      typeof plugin.statusMessage === 'function'
+        ? plugin.statusMessage()
+        : undefined
+    if (
+      _.isUndefined(statusMessage) &&
+      _.isUndefined(app.providerStatus[plugin.name]) &&
+      _.isUndefined(plugin.statusMessage)
+    ) {
+      app.setProviderStatus(plugin.name, 'Started')
+    }
+  }
+
+  function doPluginStart (app, plugin, location, configuration, restart) {
+    debug('Starting plugin %s from %s', plugin.name, location)
+    try {
+      plugin.start(configuration, restart)
+      debug('Started plugin ' + plugin.name)
+      setPluginStartedMessage(plugin)
+    } catch (e) {
+      console.error('error starting plugin: ' + e)
+      app.setProviderError(plugin.name, `Failed to start: ${e.message}`)
+    }
+  }
+
+  function doRegisterPlugin (app, packageName, metadata, location) {
     const appCopy = _.assign({}, app, {
       getSelfPath,
       getPath,
       putSelfPath,
       putPath,
       error: msg => {
-        console.error(`${pluginName}:${msg}`)
+        console.error(`${packageName}:${msg}`)
       },
-      debug: require('debug')(pluginName),
+      debug: require('debug')(packageName),
       registerDeltaInputHandler: handler => {
         onStopHandlers[plugin.id].push(app.registerDeltaInputHandler(handler))
+      },
+      setProviderStatus: msg => {
+        app.setProviderStatus(plugin.name, msg)
+      },
+      setProviderError: msg => {
+        app.setProviderError(plugin.name, msg)
       }
     })
-    const plugin = require(path.join(location, pluginName))(appCopy)
+    let plugin
+    try {
+      plugin = require(path.join(location, packageName))(appCopy)
+    } catch (e) {
+      console.error(`${packageName} failed to start: ${e.message}`)
+      app.setProviderError(packageName, `Failed to start: ${e.message}`)
+      return
+    }
     onStopHandlers[plugin.id] = []
 
     if (app.pluginsMap[plugin.id]) {
@@ -261,14 +300,18 @@ module.exports = function (app) {
           console.error(err)
         } else {
           stopPlugin(plugin)
-          plugin.start(newConfiguration, restart)
+          doPluginStart(app, plugin, location, newConfiguration, restart)
         }
       })
     }
     if (options && options.enabled) {
-      debug('Starting plugin %s from %s', pluginName, location)
-      plugin.start(getPluginOptions(plugin.id).configuration, restart)
-      debug('Started plugin ' + pluginName)
+      doPluginStart(
+        app,
+        plugin,
+        location,
+        getPluginOptions(plugin.id).configuration,
+        restart
+      )
     }
     plugin.enableLogging = options.enableLogging
     app.plugins.push(plugin)
@@ -302,9 +345,7 @@ module.exports = function (app) {
         const options = getPluginOptions(plugin.id)
         plugin.enableLogging = options.enableLogging
         if (options.enabled) {
-          debug('Starting plugin ' + pluginName)
-          plugin.start(options.configuration, restart)
-          debug('Started plugin ' + pluginName)
+          doPluginStart(app, plugin, location, options.configuration, restart)
         }
       })
     })

lib/interfaces/ws.js
@@ -134,6 +134,10 @@ module.exports = function (app) {
         if (msg.updates) {
           if (!app.securityStrategy.shouldAllowWrite(spark.request, msg)) {
             debug('security disallowed update')
+            app.setProviderError(
+              'ws',
+              spark.request.connection.remoteAddress + ' needs authentication'
+            )
             return
           }
           app.handleMessage(spark.request.source || 'ws', msg)

lib/pipedproviders.js
@@ -16,6 +16,7 @@
 
 const deep = require('deep-get-set')
 const DevNull = require('dev-null-stream')
+const _ = require('lodash')
 
 module.exports = function (app) {
   function createPipedProvider (providerConfig) {
@@ -23,7 +24,7 @@ module.exports = function (app) {
       id: providerConfig.id,
       pipeElements: providerConfig.pipeElements.reduce((result, config) => {
         if (typeof config.enabled === 'undefined' || config.enabled) {
-          result.push(createPipeElement(config))
+          result.push(createPipeElement(providerConfig.id, config))
         }
         return result
       }, [])
@@ -41,9 +42,10 @@ module.exports = function (app) {
     return result
   }
 
-  function createPipeElement (elementConfig) {
+  function createPipeElement (providerId, elementConfig) {
     var options = elementConfig.options || {}
     options.app = app
+    options.providerId = providerId
 
     if (elementConfig.optionMappings) {
       elementConfig.optionMappings.forEach(function (mapping) {

packages/server-admin-ui/src/index.js
@@ -73,6 +73,15 @@ let store = createStore(
         serverStatistics: action.data
       }
     }
+    if (action.type === 'PROVIDERSTATUS') {
+      action.data.sort( (l, r) => {
+        return l.id > r.id
+      })
+      return {
+        ...state,
+        providerStatus: action.data
+      }
+    }
     if (action.type === 'RECEIVE_LOGIN_STATUS') {
       return {
         ...state,

packages/server-admin-ui/src/views/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, CardBody, CardHeader, Progress, Row, Col } from 'reactstrap'
+import { Card, CardBody, CardHeader, Progress, Row, Col, Table } from 'reactstrap'
 import '../../fa-pulse.css'
 
 const Dashboard = props => {
@@ -8,12 +8,18 @@ const Dashboard = props => {
     deltaRate,
     numberOfAvailablePaths,
     wsClients,
-    providerStatistics
+    providerStatistics,
   } = props.serverStatistics || {
     deltaRate: 0,
     numberOfAvailablePaths: 0,
     wsClients: 0,
-    providerStatistics: {}
+    providerStatistics: {},
+  }
+  const providerStatus = props.providerStatus || []
+  const errorCount = providerStatus.filter(s => s.type === 'error').length
+  let errors = ''
+  if ( errorCount > 0 ) {
+    errors = `(${errorCount} errors)`
   }
   return (
     <div className='animated fadeIn'>
@@ -23,7 +29,7 @@ const Dashboard = props => {
             <CardHeader>Stats</CardHeader>
             <CardBody>
               <Row>
-                <Col xs='12' md='8'>
+                <Col xs='12' md='6'>
                   <div className='callout callout-primary'>
                     <small className='text-muted'>
                       Total server Signal K throughput (deltas/second)
@@ -46,7 +52,7 @@ const Dashboard = props => {
                     <strong className='h4'>{wsClients}</strong>
                   </div>
                 </Col>
-                <Col xs='12' md='4'>
+                <Col xs='12' md='6'>
                   <div className='text-muted'>
                     Provider activity (deltas/second)
                   </div>
@@ -57,7 +63,7 @@ const Dashboard = props => {
                         'icon-feed text-primary' +
                         (providerStats.deltaRate > 50
                           ? ' fa-pulse-fast'
-                          : providerStats.deltaRate > 0 ? ' fa-pulse' : '')
+                         : providerStats.deltaRate > 0 ? ' fa-pulse' : '')
                       return (
                         <li key={providerId}>
                           <i className={iconClass} />
@@ -88,6 +94,45 @@ const Dashboard = props => {
               </Row>
             </CardBody>
           </Card>
+
+        <Card>
+          <CardHeader>Provider & Plugin Status <p className='text-danger'>{errors}</p></CardHeader>
+          <CardBody>
+            <Row>
+              <Col xs='12' md='12'>
+              <Table hover responsive bordered striped size='sm'>
+                <thead>
+                  <tr>
+                  <th>Id</th>
+                  <th>Last Error</th>
+                  <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+               {providerStatus.map(status => {
+               let statusClass
+               if ( status.type === 'status' ) {
+                 statusClass = 'text-success'
+               } else if ( status.type === 'warning' ) {
+                 statusClass = 'text-warning'
+               } else {
+                 statusClass = 'text-danger'
+               }
+               const lastError = status.lastError && status.lastError != status.message ? status.lastErrorTimeStamp + ': ' + status.lastError : ''
+               return (
+                 <tr key={status.id}>
+                 <td>{status.id}</td>
+                 <td><p className='text-danger'>{lastError}</p></td>
+                 <td><p className={statusClass}>{status.message}</p></td>
+               </tr>
+               )
+               })}   
+                </tbody>
+              </Table>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
         </div>
       )}
 
@@ -100,7 +145,8 @@ const Dashboard = props => {
   )
 }
 
-export default connect(({ serverStatistics, websocketStatus }) => ({
+export default connect(({ serverStatistics, websocketStatus, providerStatus }) => ({
   serverStatistics,
-  websocketStatus
+  websocketStatus,
+  providerStatus
 }))(Dashboard)

providers/execute.js
@@ -59,9 +59,12 @@ function start (command, that) {
     that.childProcess = require('child_process').spawn('sh', ['-c', command])
   }
   that.lastStartupTime = new Date().getTime()
+  that.options.app.setProviderStatus(that.options.providerId, 'Started')
 
   that.childProcess.stderr.on('data', function (data) {
-    console.error(data.toString())
+    const msg = data.toString()
+    that.options.app.setProviderError(that.options.providerId, msg)
+    console.error(msg)
   })
 
   that.childProcess.stdout.on('data', function (data) {
@@ -69,7 +72,9 @@ function start (command, that) {
   })
 
   that.childProcess.on('close', code => {
-    debug(`process exited with ${code}`)
+    const msg = `Process exited with ${code}`
+    that.options.app.setProviderError(that.options.providerId, msg)
+    debug(msg)
     if (
       typeof that.options.restartOnClose === 'undefined' ||
       that.options.restartOnClose
@@ -81,7 +86,9 @@ function start (command, that) {
         start(command, that)
       } else {
         var nextStart = throttleTime - sinceLast
-        debug(`waiting ${nextStart / 1000} seconds to restart`)
+        const msg = `Waiting ${nextStart / 1000} seconds to restart`
+        that.options.app.setProviderError(that.options.providerId, msg)
+        debug(msg)
         setTimeout(function () {
           start(command, that)
         }, nextStart)

commit 9a680f80235b3c88fa279a788521a6d35833ce5f

feature: serial port retry logic
Add retry logic to serialport provider and fix the one-shot
retry that was broken in a732e2eba8f9e319a056b2780d9a47eaf9bda6b2.
Retry period starts at 1 second and backs off to no more than
60 seconds.

providers/serialport.js
@@ -72,6 +72,7 @@ function SerialStream (options) {
   Transform.call(this, options)
 
   this.reconnect = options.reconnect || true
+  this.reconnectDelay = 1000
   this.serial = null
   this.options = options
   this.maxPendingWrites = options.maxPendingWrites || 5
@@ -104,10 +105,12 @@ SerialStream.prototype.start = function () {
   this.serial.on(
     'open',
     function () {
+      this.reconnectDelay = 1000
       this.options.app.setProviderStatus(
         this.options.providerId,
         `Connected to ${this.options.device}`
       )
+      this.options.app.setProviderError(this.options.providerId, '')
       const parser = new SerialPort.parsers.Readline()
       this.serial.pipe(parser).pipe(this)
     }.bind(this)
@@ -117,7 +120,8 @@ SerialStream.prototype.start = function () {
     'error',
     function (x) {
       this.options.app.setProviderError(this.options.providerId, x.message)
-      console.log(x)
+      console.log(x.message)
+      this.scheduleReconnect()
     }.bind(this)
   )
   this.serial.on(
@@ -127,7 +131,7 @@ SerialStream.prototype.start = function () {
         this.options.providerId,
         'Closed, reconnecting...'
       )
-      this.start.bind(this)
+      this.scheduleReconnect()
     }.bind(this)
   )
 
@@ -163,6 +167,16 @@ SerialStream.prototype.end = function () {
 SerialStream.prototype._transform = function (chunk, encoding, done) {
   this.push(chunk)
   done()
+}
+
+SerialStream.prototype.scheduleReconnect = function () {
+  this.reconnectDelay *= this.reconnectDelay < 60 * 1000 ? 1.5 : 1
+  const msg = `Not connected (retry delay ${(
+    this.reconnectDelay / 1000
+  ).toFixed(0)} s)`
+  debug(msg)
+  this.options.app.setProviderStatus(this.options.providerId, msg)
+  setTimeout(this.start.bind(this), this.reconnectDelay)
 }
 
 module.exports = SerialStream