4395b995304e3243b84b6dbc632d81131d398972
Adrian Panazzolo <38519157+panaaj@users.noreply.github.com>
2019-12-11 11:13:36 +1030

    Updated plugin-ts-example.

--- plugin-ts-example/src/index.ts
+++ plugin-ts-example/src/index.ts
@@ -1,28 +1,44 @@
-import { Plugin, ServerAPI } from 'signalk-server/lib/interfaces/plugins'
 
-module.exports = (serverApi: ServerAPI): Plugin => {
-  return {
-    name: 'Example Plugin in TypeScript',
-    id: 'plugin-ts-example',
-    schema: () => ({
-      properties: {
-        propA: {
-          type: 'string',
-          title: 'Property A'
+import { Plugin, ServerAPI } from '../../src/interfaces/plugins';
+
+module.exports= (server: ServerAPI): Plugin=> {
+
+    let config: object;     // ** applied configuration settings          
+
+    let plugin: Plugin= { 
+        id: 'my-plugin-id',
+        name: 'My plugin (this is the name that appears in Plugin Config screen.)',
+        schema: {
+            properties: {
+                myAttribute: {
+                    type: "string",
+                    title: "Title text",
+                    default: "No value has been entered."
+                }                
+            }
         },
-        propB: {
-          type: 'number',
-          title: 'Property B'
+
+        start: function(options: any) {
+            config= options;
+            try {
+                server.debug(`** ${this.name} starting.......`);
+                // do startup actions here.	
+                server.setProviderStatus('Started');	
+            } 
+            catch(err) {
+                server.setProviderError(`Started with errors!`);
+                server.error('** EXCEPTION: **');
+                server.error(err.stack);
+                return err;
+            }   
+        },
+
+        stop: function() {
+            server.debug(`** ${this.name} stopping.......`);
+            // do clean up actions here.
+            server.setProviderStatus('Stopped');
         }
-      }
-    }),
-    start: (options: any) => {
-      serverApi.setProviderStatus(
-        `Started with options ${JSON.stringify(options)}`
-      )
-    },
-    stop: () => {
-      serverApi.setProviderStatus('Stopped')
     }
-  }
+   
+    return plugin;
 }
