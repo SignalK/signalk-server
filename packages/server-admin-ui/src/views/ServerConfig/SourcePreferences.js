153543c4dbc3591af3bbcf7cf4293e73cb42c7a9
Teppo Kurki <teppo.kurki@iki.fi>
2020-08-27 22:26:35 +0300

    render input fields including extra empties

--- packages/server-admin-ui/src/views/ServerConfig/SourcePreferences.js
+++ packages/server-admin-ui/src/views/ServerConfig/SourcePreferences.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Card, CardHeader, CardBody, CardFooter, Table } from 'reactstrap'
+import { Card, CardHeader, CardBody, CardFooter, Input, Table } from 'reactstrap'
 
 const PrefsEditor = ({ priorities }) => {
   return (
@@ -8,15 +8,30 @@ const PrefsEditor = ({ priorities }) => {
       <thead>
         <tr>
           <td>sourceRef</td>
-          <td>timeout</td>
+          <td>timeout (ms)</td>
         </tr>
       </thead>
       <tbody>
-        {priorities.map(({ sourceRef, timeout }) => {
+        {[...priorities, {sourceRef: '', timeout: ''}].map(({ sourceRef, timeout }, i) => {
           return (
-            <tr key={sourceRef}>
-              <td>{sourceRef}</td>
-              <td>{timeout} ms</td>
+            <tr key={i}>
+              <td>
+                <Input
+                  type='text'
+                  name='sourceRef'
+                  onChange={() => {}}
+                  value={sourceRef}
+                />
+
+              </td>
+              <td>
+              <Input
+                  type='number'
+                  name='timeout'
+                  onChange={() => {}}
+                  value={timeout}
+                />
+              </td>
             </tr>
           )
         })}
@@ -34,6 +49,7 @@ class SourcePreferences extends Component {
     const sourcePriorities = Object.keys(
       this.props.sourcePriorities
     ).map(key => [key, this.props.sourcePriorities[key]])
+    sourcePriorities.push(['', []])
     return (
       <Card>
         <CardHeader>Source Preferences</CardHeader>
@@ -49,7 +65,14 @@ class SourcePreferences extends Component {
               {sourcePriorities.map(([path, priorities]) => {
                 return (
                   <tr key={path}>
-                    <td>{path}</td>
+                    <td>
+                      <Input
+                        type='text'
+                        name='path'
+                        onChange={this.handleChange}
+                        value={path}
+                      />
+                    </td>
                     <td>
                       <PrefsEditor priorities={priorities} />
                     </td>
