5d773441eacbfe26c13257a54aa581ed793e4994
Teppo Kurki <teppo.kurki@iki.fi>
2020-09-21 21:52:36 +0300

    fix: plugin dashboard links
    Add app.setPluginStatus and app.setPluginError and use
    them in plugin.ts so that the status objects retain their
    origin and type.
    
    Use plugin.id instead of plugin.name as the id
    in the status data structure.

--- src/index.js
+++ src/index.js
@@ -76,6 +76,14 @@ function Server(opts) {
 
   app.providerStatus = {}
 
+  app.setPluginStatus = function(providerId, statusMessage) {
+    doSetProviderStatus(providerId, statusMessage, 'status', 'plugin')
+  }
+
+  app.setPluginError = function(providerId, errorMessage) {
+    doSetProviderStatus(providerId, errorMessage, 'error', 'plugin')
+  }
+
   app.setProviderStatus = function(providerId, statusMessage) {
     doSetProviderStatus(providerId, statusMessage, 'status')
   }
@@ -84,7 +92,7 @@ function Server(opts) {
     doSetProviderStatus(providerId, errorMessage, 'error')
   }
 
-  function doSetProviderStatus(providerId, statusMessage, type) {
+  function doSetProviderStatus(providerId, statusMessage, type, statusType = 'provider') {
     if (!statusMessage) {
       delete app.providerStatus[providerId]
       return
@@ -102,10 +110,11 @@ function Server(opts) {
 
     status.type = type
     status.id = providerId
-    status.statusType = 'provider'
+    status.statusType = statusType
     status.timeStamp = new Date().toLocaleString()
 
     status.message = statusMessage
+    console.log(app.getProviderStatus())
 
     app.emit('serverevent', {
       type: 'PROVIDERSTATUS',
