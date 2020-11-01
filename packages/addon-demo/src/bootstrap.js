0be25901c45713069160f9e515d5303bf0d2d825
Teppo Kurki <teppo.kurki@iki.fi>
2020-11-01 22:58:38 +0200

    remove unnecessary bootstrap

--- packages/addon-demo/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Hello from second app</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
