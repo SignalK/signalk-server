SignalK/signalk-server

commit 287b15ed5e0516d7bd7f9e283687bc28552cc5b6

feature: move server management endpoints under /skServer (#1161)
* feature: move server management endpoints under /skServer

* fix: support PUT to /security/access/requests for backwards compatibly

Co-authored-by: Scott Bender <scott@scottbender.net>

src/interfaces/plugins.ts
@@ -22,6 +22,7 @@ import express from 'express'
 import fs from 'fs'
 import _ from 'lodash'
 import path from 'path'
+import { SERVERROUTESPREFIX } from '../constants'
 import { listAllSerialPorts, Ports } from '../serialports'
 
 // tslint:disable-next-line:no-var-requires
@@ -134,31 +135,34 @@ module.exports = (theApp: any) => {
       ensureExists(path.join(theApp.config.configPath, 'plugin-config-data'))
 
       theApp.use(
-        '/plugins/configure',
+        `${SERVERROUTESPREFIX}/plugins/configure`,
         express.static(getPluginConfigPublic(theApp))
       )
 
       const router = express.Router()
 
-      theApp.get('/plugins', (req: Request, res: Response) => {
-        const providerStatus = theApp.getProviderStatus()
+      theApp.get(
+        `${SERVERROUTESPREFIX}/plugins`,
+        (req: Request, res: Response) => {
+          const providerStatus = theApp.getProviderStatus()
 
-        Promise.all(
-          _.sortBy(theApp.plugins, [
-            (plugin: PluginInfo) => {
-              return plugin.name
-            }
-          ]).map((plugin: PluginInfo) =>
-            getPluginResponseInfo(plugin, providerStatus)
+          Promise.all(
+            _.sortBy(theApp.plugins, [
+              (plugin: PluginInfo) => {
+                return plugin.name
+              }
+            ]).map((plugin: PluginInfo) =>
+              getPluginResponseInfo(plugin, providerStatus)
+            )
           )
-        )
-          .then(json => res.json(json))
-          .catch(err => {
-            console.error(err)
-            res.status(500)
-            res.send(err)
-          })
-      })
+            .then(json => res.json(json))
+            .catch(err => {
+              console.error(err)
+              res.status(500)
+              res.send(err)
+            })
+        }
+      )
     }
   }
 
@@ -614,10 +618,13 @@ module.exports = (theApp: any) => {
     if (typeof plugin.registerWithRouter !== 'undefined') {
       plugin.registerWithRouter(router)
     }
-    app.use('/plugins/' + plugin.id, router)
+    app.use(`${SERVERROUTESPREFIX}/plugins/` + plugin.id, router)
 
     if (typeof plugin.signalKApiRoutes === 'function') {
-      app.use('/signalk/v1/api', plugin.signalKApiRoutes(express.Router()))
+      app.use(
+        `${SERVERROUTESPREFIX}/signalk/v1/api`,
+        plugin.signalKApiRoutes(express.Router())
+      )
     }
   }
 }

packages/server-admin-ui/src/actions.js
@@ -1,3 +1,5 @@
+import {isUndefined} from 'lodash'
+
 const authFetch = (url, options) => {
   return fetch(url, {
     ...options,
@@ -82,7 +84,7 @@ export function enableSecurity (dispatch, userId, password, callback) {
     password: password,
     type: 'admin'
   }
-  fetch('/enableSecurity', {
+  fetch(`${window.serverRoutesPrefix}/enableSecurity`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -102,7 +104,7 @@ export function enableSecurity (dispatch, userId, password, callback) {
 export function restart () {
   return dispatch => {
     if (confirm('Are you sure you want to restart?')) {
-      fetch('/restart', {
+      fetch(`${window.serverRoutesPrefix}/restart`, {
         credentials: 'include',
         method: 'PUT'
       }).then(() => {
@@ -113,8 +115,8 @@ export function restart () {
 }
 
 // Build actions that perform a basic authFetch to the backend. Pull #514.
-export const buildFetchAction = (endpoint, type) => dispatch =>
-  authFetch(endpoint)
+export const buildFetchAction = (endpoint, type, prefix) => dispatch =>
+  authFetch(`${isUndefined(prefix) ? window.serverRoutesPrefix : prefix}${endpoint}`)
     .then(response => response.json())
     .then(data =>
       dispatch({
@@ -128,7 +130,7 @@ export const fetchPlugins = buildFetchAction('/plugins', 'RECEIVE_PLUGIN_LIST')
 export const fetchWebapps = buildFetchAction('/webapps', 'RECEIVE_WEBAPPS_LIST')
 export const fetchApps = buildFetchAction('/appstore/available', 'RECEIVE_APPSTORE_LIST')
 export const fetchAccessRequests = buildFetchAction('/security/access/requests', 'ACCESS_REQUEST')
-export const fetchServerSpecification = buildFetchAction('/signalk', 'RECEIVE_SERVER_SPEC')
+export const fetchServerSpecification = buildFetchAction('/signalk', 'RECEIVE_SERVER_SPEC', '')
 
 export function fetchAllData (dispatch) {
   fetchPlugins(dispatch)

packages/server-admin-ui/src/views/Configuration/Configuration.js
@@ -19,7 +19,7 @@ class Dashboard extends Component {
   }
 
   componentDidMount() {
-    fetch(`/plugins`, {credentials: 'same-origin'}).then((response) => {
+    fetch(`${window.serverRoutesPrefix}/plugins`, {credentials: 'same-origin'}).then((response) => {
       if (response.status == 200) {
         return response.json()
       } else {
@@ -58,7 +58,7 @@ class Dashboard extends Component {
 }
 
 function saveData(id, data) {
-  fetch('/plugins/' + id + "/config", {
+  fetch(`${window.serverRoutesPrefix}/plugins/${id}/config`, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: new Headers({'Content-Type': 'application/json'}),

packages/server-admin-ui/src/views/Playground.js
@@ -118,7 +118,7 @@ class Playground extends Component {
     if ( sendToServer ) {
       this.setState({...this.state, sending: true})
     }
-    fetch(`/skServer/inputTest`, {
+    fetch(`${window.serverRoutesPrefix}/inputTest`, {
       method: 'POST',
       credentials: 'include',
       headers: {

packages/server-admin-ui/src/views/ServerConfig/BackupRestore.js
@@ -51,13 +51,13 @@ class BackupRestore extends Component {
   }
 
   backup() {
-    const url = `/backup?includePlugins=${this.state.includePlugins}`
+    const url = `${window.serverRoutesPrefix}/backup?includePlugins=${this.state.includePlugins}`
     //const url = `/backup`
     window.location = url
   }
 
   restore() {
-    fetch(`/restore`, {
+    fetch(`${window.serverRoutesPrefix}/restore`, {
       credentials: 'include',
       method: 'POST',
       headers: {
@@ -100,7 +100,7 @@ class BackupRestore extends Component {
     data.append('file', this.state.restoreFile)
     
     this.setState({restoreState: RESTORE_VALIDATING})
-    fetch(`/validateBackup`, {
+    fetch(`${window.serverRoutesPrefix}/validateBackup`, {
       credentials: 'include',
       method: 'POST',
       headers: {

packages/server-admin-ui/src/views/ServerConfig/BasicProvider.js
@@ -167,7 +167,7 @@ class DeviceInput extends Component {
   }
 
   componentDidMount() {
-    fetch(`/serialports`, {
+    fetch(`${window.serverRoutesPrefix}/serialports`, {
       credentials: 'include'
     })
       .then(response => response.json())

packages/server-admin-ui/src/views/ServerConfig/Logging.js
@@ -13,7 +13,7 @@ import {
 } from 'reactstrap'
 
 function fetchLogfileList () {
-  const okResponse = fetch(`/logfiles/`, {
+  const okResponse = fetch(`${window.serverRoutesPrefix}/logfiles/`, {
     credentials: 'include'
   }).then(response => {
     if (!response.ok) {

packages/server-admin-ui/src/views/ServerConfig/ProvidersConfiguration.js
@@ -29,7 +29,7 @@ import SourcePriorities from './SourcePriorities'
 import { set } from 'lodash'
 
 function fetchProviders () {
-  fetch(`/providers`, {
+  fetch(`${window.serverRoutesPrefix}/providers`, {
     credentials: 'include'
   })
     .then(response => response.json())
@@ -52,7 +52,7 @@ function fetchProviders () {
 }
 
 function runDiscovery () {
-  fetch(`/runDiscovery`, {
+  fetch(`${window.serverRoutesPrefix}/runDiscovery`, {
     method: 'PUT',
     credentials: 'include'
   })
@@ -139,7 +139,7 @@ class ProvidersConfiguration extends Component {
 
     var id = this.state.selectedProvider.originalId
 
-    fetch(`/providers/${id && !isNew ? id : ''}`, {
+    fetch(`${window.serverRoutesPrefix}/providers/${id && !isNew ? id : ''}`, {
       method: isNew ? 'POST' : 'PUT',
       headers: {
         'Content-Type': 'application/json'
@@ -182,7 +182,7 @@ class ProvidersConfiguration extends Component {
   }
 
   handleDelete (event) {
-    fetch(`/providers/${this.state.selectedProvider.id}`, {
+    fetch(`${window.serverRoutesPrefix}/providers/${this.state.selectedProvider.id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'

commit 3002a71ea11c07371b7023642538d683a4fe1766

fix: broken & incorrectly fixed signalKApiRoutes
Originally broken in 287b15ed5e0516d7bd7f9e283687bc28552cc5b6 and
the subsequent fix worked by chance, not by design.

src/interfaces/plugins.ts
@@ -627,10 +627,7 @@ module.exports = (theApp: any) => {
     app.use(backwardsCompat('/plugins/' + plugin.id), router)
 
     if (typeof plugin.signalKApiRoutes === 'function') {
-      app.use(
-        backwardsCompat('/signalk/v1/api'),
-        plugin.signalKApiRoutes(express.Router())
-      )
+      app.use('/signalk/v1/api', plugin.signalKApiRoutes(express.Router()))
     }
   }
 }