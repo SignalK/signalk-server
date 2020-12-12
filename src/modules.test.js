a6225a9237386192f4fe2664544d1bf54316433b
Teppo Kurki <teppo.kurki@iki.fi>
2020-12-12 17:23:44 +0200

    fix: failing test since removal of maptracker

--- src/modules.test.js
+++ src/modules.test.js
@@ -8,52 +8,54 @@ const {
   getLatestServerVersion
 } = require('./modules')
 
-const expectedModules = [
-  '@signalk/freeboard-sk',
-  '@signalk/instrumentpanel',
-  '@signalk/maptracker',
-  '@signalk/sailgauge',
-  '@signalk/simplegauges'
-]
-
-const testTempDir = path.join(
-  require('os').tmpdir(),
-  '_skservertest_modules' + Date.now()
-)
-
-const app = {
-  config: {
-    appPath: path.join(__dirname + '/../'),
-    configPath: testTempDir
-  }
-}
-
-fs.mkdirSync(testTempDir)
-const tempNodeModules = path.join(testTempDir, 'node_modules/')
-fs.mkdirSync(path.join(testTempDir, 'node_modules'))
-fs.mkdirSync(path.join(testTempDir, 'node_modules/@signalk'))
-const configMaptrackerDirectory = path.join(
-  testTempDir,
-  'node_modules/@signalk/maptracker'
-)
-fs.mkdirSync(configMaptrackerDirectory)
-
-const maptrackerPkg = require(path.join(
-  app.config.appPath,
-  'node_modules/@signalk/maptracker/package.json'
-))
-maptrackerPkg.version = '1000.0.0'
-fs.writeFileSync(
-  path.join(configMaptrackerDirectory, 'package.json'),
-  JSON.stringify(maptrackerPkg)
-)
 
 describe('modulesWithKeyword', () => {
-  it('returns a list of modules', () => {
+  it('returns a list of modules with one "installed" update in config dir', () => {
+    const expectedModules = [
+      '@signalk/freeboard-sk',
+      '@signalk/instrumentpanel',
+      '@signalk/sailgauge',
+      '@signalk/simplegauges'
+    ]
+    const updateInstalledModule = '@signalk/instrumentpanel'
+    const indexOfInstalledModule = expectedModules.indexOf(updateInstalledModule)
+
+    const testTempDir = path.join(
+      require('os').tmpdir(),
+      '_skservertest_modules' + Date.now()
+    )
+
+    const app = {
+      config: {
+        appPath: path.join(__dirname + '/../'),
+        configPath: testTempDir
+      }
+    }
+
+    fs.mkdirSync(testTempDir)
+    const tempNodeModules = path.join(testTempDir, 'node_modules/')
+    fs.mkdirSync(path.join(testTempDir, 'node_modules'))
+    fs.mkdirSync(path.join(testTempDir, 'node_modules/@signalk'))
+    const installedModuleDirectory = path.join(
+      testTempDir,
+      `node_modules/${updateInstalledModule}`
+    )
+    fs.mkdirSync(installedModuleDirectory)
+
+    const fakeInstalledModulePackageJson = require(path.join(
+      app.config.appPath,
+      `node_modules/${updateInstalledModule}/package.json`
+    ))
+    fakeInstalledModulePackageJson.version = '1000.0.0'
+    fs.writeFileSync(
+      path.join(installedModuleDirectory, 'package.json'),
+      JSON.stringify(fakeInstalledModulePackageJson)
+    )
+
     const moduleList = modulesWithKeyword(app, 'signalk-webapp')
     chai.expect(_.map(moduleList, 'module')).to.eql(expectedModules)
     chai.expect(moduleList[0].location).to.not.eql(tempNodeModules)
-    chai.expect(moduleList[2].location).to.eql(tempNodeModules)
+    chai.expect(moduleList[indexOfInstalledModule].location).to.eql(tempNodeModules)
   })
 })
 
