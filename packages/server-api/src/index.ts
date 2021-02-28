2c3f1d741c2e282474dd1f40fd4d3bf5a42e41a9
Teppo Kurki <teppo.kurki@iki.fi>
2021-02-28 22:13:18 +0200

    doc, refactor: rename & expand Plugin interface

--- packages/server-api/src/index.ts
+++ packages/server-api/src/index.ts
@@ -1,4 +1,5 @@
-import { PropertyValues, PropertyValuesCallback } from './propertyvalues';
+import { IRouter } from 'express'
+import { PropertyValues, PropertyValuesCallback } from './propertyvalues'
 
 export * from './propertyvalues'
 
@@ -10,7 +11,7 @@ type Unsubscribe = () => {}
 /**
  * This is the API that a server exposes in the app object that
  * is passed in Plugin "constructor" call.
- * 
+ *
  * INCOMPLETE, work in progress.
  */
 export interface PluginServerAPI {
@@ -19,10 +20,39 @@ export interface PluginServerAPI {
 }
 
 /**
- * This is the API that a plugin must implement.
- * 
- * INCOMPLETE, work in progress 
+ * This is the API that a [server plugin](https://github.com/SignalK/signalk-server/blob/master/SERVERPLUGINS.md) must implement.
+ *
+ * Typing is INCOMPLETE.
  */
-export interface PluginAPI {
+export interface Plugin {
+  /**
+   * Used to identify the plugin in the server, for example
+   * when storing the plugin's configuration and in http endpoints.
+   */
+  id: string
 
+  /**
+   * Human oriented name for the plugin. This is used in the
+   * server's plugin configuration user interface.
+   */
+  name: string
+
+  /**
+   * Called to start the plugin with latest saved configuration. Called
+   * - for enabled (by configuration or by default) plugins during server startup
+   * - after stop() when the configuration of an enabled plugin has been updated
+   * in the admin UI
+   * - when a plugin is Enabled in the admin UI
+   */
+  start: (config: object, restart: (newConfiguration: object) => void) => any
+
+  /**
+   * Called to stop the plugin. Called when the user disables the plugin in the admin UI.
+   */
+  stop: () => void
+  schema: () => object | object
+  uiSchema?: () => object | object
+  registerWithRouter?: (router: IRouter) => void
+  signalKApiRoutes?: (router: IRouter) => IRouter
+  enabledByDefault?: boolean
 }
