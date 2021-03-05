6d59ef788f9d6261b6f20bacc2fd9780df0b0d47
Scott Bender <scott@scottbender.net>
2021-03-04 22:31:23 -0500

    fix: label vertical alignment

--- packages/server-admin-ui/src/views/Configuration/Configuration.js
+++ packages/server-admin-ui/src/views/Configuration/Configuration.js
@@ -83,7 +83,7 @@ export default class PluginConfigurationList extends Component {
           >
 
           <FormGroup row>
-          <Col xs='3' md='1'>
+          <Col xs='3' md='1' className={'col-form-label'}>
             <Label htmlFor='select'>Search</Label>
           </Col>
           <Col xs='12' md='4'>
