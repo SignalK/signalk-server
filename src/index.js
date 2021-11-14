b8eefceeec022efe3e5ad796eb9db8dbadbea817
panaaj <38519157+panaaj@users.noreply.github.com>
2021-11-15 10:10:21 +1030

    add constructor

--- src/index.js
+++ src/index.js
@@ -71,8 +71,7 @@ function Server(opts) {
   require('./put').start(app)
 
   // ** initialise resources API **
-  app.resourcesApi= new Resources()
-  app.resourcesApi.start(app)
+  app.resourcesApi= new Resources(app)
 
   app.signalk = new FullSignalK(app.selfId, app.selfType)
 
