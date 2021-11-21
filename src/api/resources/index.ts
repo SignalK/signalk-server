bf6da860d7db8004d1df8625834b9dce3a6ef2cb
Teppo Kurki <teppo.kurki@iki.fi>
2021-11-21 10:21:25 +0200

    refactor: use Express types

--- src/api/resources/index.ts
+++ src/api/resources/index.ts
@@ -4,6 +4,7 @@ import { buildResource } from './resources'
 import { validate } from './validate'
 
 import { SignalKResourceType, ResourceProvider, ResourceProviderMethods } from '@signalk/server-api'
+import { Application, Handler, NextFunction, Request, Response } from 'express'
 
 const debug = Debug('signalk:resources')
 
@@ -30,9 +31,13 @@ const API_METHODS = [
   'deleteRegion'
 ]
 
+// FIXME use types from https://github.com/SignalK/signalk-server/pull/1358
+interface ResourceApplication extends Application {
+  handleMessage: any
+}
 export class Resources {
   private resProvider: { [key: string]: ResourceProviderMethods | null } = {}
-  private server: any
+  private server: ResourceApplication
 
   // in-scope resource types
   private resourceTypes: SignalKResourceType[] = [
@@ -43,7 +48,8 @@ export class Resources {
     'charts'
   ]
 
-  constructor(app: any) {
+  constructor(app: ResourceApplication) {
+    this.server = app
     this.start(app)
   }
 
@@ -97,7 +103,7 @@ export class Resources {
 
   private initResourceRoutes() {
     // list all serviced paths under resources
-    this.server.get(`${SIGNALK_API_PATH}/resources`, (req: any, res: any) => {
+    this.server.get(`${SIGNALK_API_PATH}/resources`, (req: Request, res: Response) => {
       res.json(this.getResourcePaths())
     })
 
@@ -122,6 +128,7 @@ export class Resources {
     )
 
     this.server.use(
+<<<<<<< HEAD
       `${SIGNALK_API_PATH}/resources/:resourceType/:resourceId`,
       async (req: any, res: any, next: any) => {
         const result = this.parseResourceRequest(req)
@@ -144,6 +151,10 @@ export class Resources {
     this.server.use(
       `${SIGNALK_API_PATH}/resources/:resourceType`,
       async (req: any, res: any, next: any) => {
+=======
+      `${SIGNALK_API_PATH}/resources/*`,
+      async (req: Request, res: Response, next: NextFunction) => {
+>>>>>>> refactor: use Express types
         const result = this.parseResourceRequest(req)
         if (result) {
           const ar = await this.actionResourceRequest(result)
@@ -185,8 +196,14 @@ export class Resources {
     return resPaths
   }
 
+<<<<<<< HEAD
   private parseResourceRequest(req: any): ResourceRequest | undefined {
     debug('********* parse request *************')
+=======
+  // parse api path request and return ResourceRequest object
+  private parseResourceRequest(req: Request): ResourceRequest | undefined {
+    debug('** req.originalUrl:', req.originalUrl)
+>>>>>>> refactor: use Express types
     debug('** req.method:', req.method)
     debug('** req.body:', req.body)
     debug('** req.query:', req.query)
