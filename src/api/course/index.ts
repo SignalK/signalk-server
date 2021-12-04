e7740fef641293b70b69416e78763653ad8e85bc
panaaj <38519157+panaaj@users.noreply.github.com>
2021-12-05 10:26:47 +1030

    update detlas

--- src/api/course/index.ts
+++ src/api/course/index.ts
@@ -9,6 +9,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Debug from 'debug'
 import { Application, Request, Response } from 'express'
 =======
@@ -89,6 +90,10 @@ import Debug from 'debug'
 import { Application, Request, Response } from 'express'
 import { v4 as uuidv4 } from 'uuid'
 >>>>>>> init courseApi
+=======
+import Debug from 'debug'
+import { Application, Request, Response } from 'express'
+>>>>>>> update detlas
 
 const debug = Debug('signalk:courseApi')
 
@@ -126,13 +131,10 @@ const API_METHODS: string[] = []
 =======
 >>>>>>> update detlas
 
-const UUID_PREFIX: string = 'urn:mrn:signalk:uuid:'
-
-const API_METHODS: string[] = []
-
 interface CourseApplication extends Application {
   handleMessage: (id: string, data: any) => void
   getSelfPath: (path: string) => any
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -208,6 +210,9 @@ interface CourseApplication extends Application {
 =======
 
 >>>>>>> init courseApi
+=======
+  registerPutHandler: (context:string, path:string, cb:any) => any
+>>>>>>> update detlas
   resourcesApi: {
     getResource: (resourceType: string, resourceId: string) => any
   }
@@ -394,10 +399,13 @@ export class CourseApi {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> enable put processing
 =======
 =======
+=======
+>>>>>>> update detlas
 =======
 >>>>>>> update detlas
 =======
@@ -412,6 +420,7 @@ export class CourseApi {
       ); 
     }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> update detlas
@@ -545,12 +554,18 @@ export class CourseApi {
 >>>>>>> update detlas
 =======
 >>>>>>> init courseApi
+=======
+>>>>>>> update detlas
     // restart / arrivalCircle
     this.server.put(
       `${COURSE_API_PATH}/:action`,
       async (req: Request, res: Response) => {
         debug(`** PUT ${COURSE_API_PATH}/:action`)
         if (req.params.restart) {
+          //test for active destination
+          if (!this.courseInfo.nextPoint.position) {
+            return
+          }
           // set previousPoint to vessel position
 >>>>>>> init courseApi
           const position: any = this.server.getSelfPath('navigation.position')
@@ -1255,6 +1270,12 @@ export class CourseApi {
 >>>>>>> init courseApi
       }
     )
+  }
+
+  private handleCourseApiPut(context:string, path:string, value:any, cb:any) {
+
+    debug('** PUT handler **')
+    return undefined
   }
 
   private async activateRoute(route: ActiveRoute): Promise<boolean> {
