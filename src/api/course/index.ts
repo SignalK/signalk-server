3aa6a250b9be7a141e48d5dec33da76e7f768076
panaaj <38519157+panaaj@users.noreply.github.com>
2021-12-12 14:29:38 +1030

    chore: lint

--- src/api/course/index.ts
+++ src/api/course/index.ts
@@ -131,7 +131,7 @@ const API_METHODS: string[] = []
 =======
 >>>>>>> update detlas
 
-const DELTA_INTERVAL: number = 30000 
+const DELTA_INTERVAL: number = 30000
 
 interface CourseApplication extends Application {
   handleMessage: (id: string, data: any) => void
@@ -329,6 +329,7 @@ export class CourseApi {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     setInterval(() => {
       if (this.courseInfo.nextPoint.position) {
         this.emitCourseInfo()
@@ -376,6 +377,10 @@ export class CourseApi {
 >>>>>>> add 30sec delta interval
     setInterval( ()=> {
       if(this.courseInfo.nextPoint.position) {
+=======
+    setInterval(() => {
+      if (this.courseInfo.nextPoint.position) {
+>>>>>>> chore: lint
         this.emitCourseInfo()
       }
     }, DELTA_INTERVAL)
@@ -1763,7 +1768,8 @@ export class CourseApi {
       newCourse.nextPoint.arrivalCircle = dest.arrivalCircle
     }
 
-    newCourse.nextPoint.type = typeof dest.type !== 'undefined' ? dest.type : null
+    newCourse.nextPoint.type =
+      typeof dest.type !== 'undefined' ? dest.type : null
 
     if (dest.href) {
       newCourse.href = dest.href
