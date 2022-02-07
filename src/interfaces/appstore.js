SignalK/signalk-server

commit b47c45eb36dd3bc9b71fce965042e096f201bf9d

refactor: depend on Config, not the whole App

src/interfaces/appstore.js
@@ -360,7 +360,7 @@ module.exports = function(app) {
     const fn = isRemove ? removeModule : installModule
 
     fn(
-      app,
+      app.config,
       module,
       version,
       output => {

src/interfaces/plugins.ts
@@ -295,7 +295,7 @@ module.exports = (theApp: any) => {
   function startPlugins(app: any) {
     app.plugins = []
     app.pluginsMap = {}
-    modulesWithKeyword(app, 'signalk-node-server-plugin').forEach(
+    modulesWithKeyword(app.config, 'signalk-node-server-plugin').forEach(
       (moduleData: any) => {
         registerPlugin(
           app,

src/interfaces/webapps.js
@@ -48,7 +48,7 @@ module.exports = function(app) {
 
 function mountWebModules(app, keyword) {
   debug(`mountWebModules:${keyword}`)
-  const modules = modulesWithKeyword(app, keyword)
+  const modules = modulesWithKeyword(app.config, keyword)
   modules.forEach(moduleData => {
     let webappPath = path.join(moduleData.location, moduleData.module)
     if (fs.existsSync(webappPath + '/public/')) {

src/modules.test.js
@@ -51,7 +51,7 @@ describe('modulesWithKeyword', () => {
       JSON.stringify(fakeInstalledModulePackageJson)
     )
 
-    const moduleList = modulesWithKeyword(app, 'signalk-webapp')
+    const moduleList = modulesWithKeyword(app.config, 'signalk-webapp')
     chai.expect(_.map(moduleList, 'module')).to.eql(expectedModules)
     chai.expect(moduleList[0].location).to.not.eql(tempNodeModules)
     chai

src/modules.ts
@@ -114,11 +114,11 @@ const priorityPrefix = (a: ModuleData, b: ModuleData) =>
   getModuleSortName(a).localeCompare(getModuleSortName(b))
 
 // Searches for installed modules that contain `keyword`.
-function modulesWithKeyword(app: WithConfig, keyword: string) {
+function modulesWithKeyword(config: Config, keyword: string) {
   return _.uniqBy(
     // _.flatten since values are inside an array. [[modules...], [modules...]]
     _.flatten(
-      getModulePaths(app.config).map(pathOption =>
+      getModulePaths(config).map(pathOption =>
         findModulesInDir(pathOption, keyword)
       )
     ),
@@ -126,38 +126,38 @@ function modulesWithKeyword(app: WithConfig, keyword: string) {
   ).sort(priorityPrefix)
 }
 function installModule(
-  app: WithConfig,
+  config: Config,
   name: string,
   version: string,
   onData: () => any,
   onErr: (err: Error) => any,
   onClose: (code: number) => any
 ) {
-  runNpm(app, name, version, 'install', onData, onErr, onClose)
+  runNpm(config, name, version, 'install', onData, onErr, onClose)
 }
 
 function removeModule(
-  app: WithConfig,
+  config: Config,
   name: string,
   version: any,
   onData: () => any,
   onErr: (err: Error) => any,
   onClose: (code: number) => any
 ) {
-  runNpm(app, name, null, 'remove', onData, onErr, onClose)
+  runNpm(config, name, null, 'remove', onData, onErr, onClose)
 }
 
 function restoreModules(
-  app: WithConfig,
+  config: Config,
   onData: () => any,
   onErr: (err: Error) => any,
   onClose: (code: number) => any
 ) {
-  runNpm(app, null, null, 'remove', onData, onErr, onClose)
+  runNpm(config, null, null, 'remove', onData, onErr, onClose)
 }
 
 function runNpm(
-  app: WithConfig,
+  config: Config,
   name: any,
   version: any,
   command: string,
@@ -178,7 +178,7 @@ function runNpm(
 
   debug(`${command}: ${packageString}`)
 
-  if (isTheServerModule(name, app)) {
+  if (isTheServerModule(name, config)) {
     if (process.platform === 'win32') {
       npm = spawn(
         'cmd',
@@ -193,7 +193,7 @@ function runNpm(
       )
     }
   } else {
-    opts.cwd = app.config.configPath
+    opts.cwd = config.configPath
 
     if (process.platform === 'win32') {
       npm = spawn('cmd', ['/c', `npm --save ${command} ${packageString}`], opts)
@@ -211,8 +211,8 @@ function runNpm(
   })
 }
 
-function isTheServerModule(moduleName: string, app: WithConfig) {
-  return moduleName === app.config.name
+function isTheServerModule(moduleName: string, config: Config) {
+  return moduleName === config.name
 }
 
 function findModulesWithKeyword(keyword: string) {

src/serverroutes.js
@@ -865,7 +865,7 @@ module.exports = function(app, saveSecurityConfig, getSecurityConfig) {
           sendRestoreStatus('Installing Plugins', '', 1)
 
           restoreModules(
-            app,
+            app.config,
             output => {
               sendRestoreStatus('Installing Plugins', `${output}`, 1)
               console.log(`stdout: ${output}`)

commit 2097c90f84c6b657001538034a7854c680010d3e

fix: correct signature for isTheServerModule
Broken in b47c45eb.d

src/interfaces/appstore.js
@@ -59,7 +59,7 @@ module.exports = function(app) {
           findPluginsAndWebapps()
             .then(([plugins, webapps]) => {
               if (
-                !isTheServerModule(name, app) &&
+                !isTheServerModule(name, app.config) &&
                 !plugins.find(packageNameIs(name)) &&
                 !webapps.find(packageNameIs(name))
               ) {
@@ -230,7 +230,7 @@ module.exports = function(app) {
         }
       }
     } else {
-      all.canUpdateServer = false
+      all.canUpdateServer = true
     }
 
     getModulesInfo(plugins, getPlugin, all)
@@ -325,7 +325,7 @@ module.exports = function(app) {
   }
 
   function installSKModule(module, version) {
-    if (isTheServerModule(module, app)) {
+    if (isTheServerModule(module, app.config)) {
       try {
         app.providers.forEach(providerHolder => {
           if (