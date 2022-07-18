fbb9fae31bff1431c302becf4dccfbcf5331202c
panaaj <38519157+panaaj@users.noreply.github.com>
2022-07-18 15:01:42 +0930

    add parameters definition

--- src/api/security/openApi.ts
+++ src/api/security/openApi.ts
@@ -2,6 +2,6 @@ import securityApiDoc from './openApi.json'
 
 export const securityApiRecord = {
   name: 'security',
-  path: '/signalk/v1/api',
+  path: '/signalk/v1',
   apiDoc: securityApiDoc
 }
