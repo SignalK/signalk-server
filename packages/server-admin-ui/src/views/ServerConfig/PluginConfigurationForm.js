331989a55b5e21175e136b1c860b11bb91f4e1ad
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2022-12-26 23:22:54 +0200

    Update PluginConfigurationForm.js

--- packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
+++ packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Form from '@rjsf/core'
+import Form from '@rjsf/bootstrap-4'
 
 export default ({ plugin, onSubmit }) => {
   const schema = JSON.parse(JSON.stringify(plugin.schema))
