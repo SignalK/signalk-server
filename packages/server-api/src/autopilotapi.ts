57568338953ba19fe86542fa13b8ad58b0bb35a0
Teppo Kurki <teppo.kurki@iki.fi>
2023-11-02 21:38:56 +0200

    convert to methods
    Convert the funky properties with function types to
    methods.

--- packages/server-api/src/autopilotapi.ts
+++ packages/server-api/src/autopilotapi.ts
@@ -1,24 +1,29 @@
 export interface AutopilotApi {
-  register: (pluginId: string, provider: AutopilotProvider, prmary?: boolean) => void
-  unRegister: (pluginId: string) => void
+  register(
+    pluginId: string,
+    provider: AutopilotProvider,
+    prmary?: boolean
+  ): void
+  unRegister(pluginId: string): void
 }
 
 export type TackGybeDirection = 'port' | 'starboard'
 
+/** @see {isAutopilotProvider} ts-auto-guard:type-guard */
 export interface AutopilotProvider {
   pilotType: string
-  getData: () => Promise<AutopilotInfo>
-  getState: () => Promise<string>
-  setState: (state: string) => Promise<void>
-  getMode: () => Promise<string>
-  setMode: (mode: string) => Promise<void>
-  getTarget: () => Promise<number>
-  setTarget: (value: number) => Promise<void>
-  adjustTarget: (value: number) => Promise<void>
-  engage: () => Promise<void>
-  disengage: () => Promise<void>
-  tack: (direction: TackGybeDirection) => Promise<void>
-  gybe: (direction: TackGybeDirection) => Promise<void>
+  getData(): Promise<AutopilotInfo>
+  getState(): Promise<string>
+  setState(state: string): Promise<void>
+  getMode(): Promise<string>
+  setMode(mode: string): Promise<void>
+  getTarget(): Promise<number>
+  setTarget(value: number): Promise<void>
+  adjustTarget(value: number): Promise<void>
+  engage(): Promise<void>
+  disengage(): Promise<void>
+  tack(direction: TackGybeDirection): Promise<void>
+  gybe(direction: TackGybeDirection): Promise<void>
 }
 
 export interface AutopilotOptions {
@@ -35,5 +40,5 @@ export interface AutopilotInfo {
 }
 
 export interface AutopilotProviderRegistry {
-  registerAutopilotProvider: (provider: AutopilotProvider) => void
+  registerAutopilotProvider(provider: AutopilotProvider): void
 }
