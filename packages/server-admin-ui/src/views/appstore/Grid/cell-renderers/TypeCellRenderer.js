f42ed025c3f2d1f21e3f47935e6cb671b067b7fd
Teppo Kurki <teppo.kurki@iki.fi>
2024-03-29 08:09:23 +0200

    simplify, useState not needed

--- packages/server-admin-ui/src/views/appstore/Grid/cell-renderers/TypeCellRenderer.js
+++ packages/server-admin-ui/src/views/appstore/Grid/cell-renderers/TypeCellRenderer.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
 
 export default function TypeCellRenderer(props) {
-  const [plugin, setPlugin] = useState(props.data.isPlugin)
-  const [app, setApp] = useState(props.data.isWebapp)
   return (
     <div className="cell__renderer cell-type center">
-      {plugin && <span className="tag">Plugin</span>}
-      {app && <span className="tag">App</span>}
+      {props.data.isPlugin && <span className="tag">Plugin</span>}
+      {props.data.isWebapp && <span className="tag">App</span>}
     </div>
   )
 }
