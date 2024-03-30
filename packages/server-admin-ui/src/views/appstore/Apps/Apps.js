05aa7f2e872820f1ceb5c7ba097d19d2f86cfe18
leonardgable <leonard.gable@outlook.com>
2024-03-30 20:29:58 +0100

    Reconfigured webpack and added dependencies version to avoid warning

--- packages/server-admin-ui/src/views/appstore/Apps/Apps.js
+++ packages/server-admin-ui/src/views/appstore/Apps/Apps.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react'
+import React, { useState, useCallback, useRef, useEffect } from 'react'
 import { connect } from 'react-redux'
 import {
   Card,
