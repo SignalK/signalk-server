dde17059b1bf1c40ba4b66df07cca9c58eeee6c3
leonardgable <leonard.gable@outlook.com>
2024-04-21 13:24:31 +0200

    added comment on the updated Substring

--- packages/server-admin-ui/src/views/appstore/Grid/cell-renderers/VersionCellRenderer.js
+++ packages/server-admin-ui/src/views/appstore/Grid/cell-renderers/VersionCellRenderer.js
@@ -7,7 +7,9 @@ export default function NameCellRenderer(props) {
         <span className="version">
           v{props.data.installedVersion || props.data.version}
         </span>
-        {/* &nbsp; ({props.data.updated.substring(0, 10)}) */}
+        {/* 
+         // TODO: Maybe think about a better way to display this information
+        &nbsp; ({props.data.updated.substring(0, 10)}) */}
         {props.data.newVersion && (
           <span className="version version--update">
             v{props.data.newVersion}
