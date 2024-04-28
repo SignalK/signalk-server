0f24f7277506347fdd02a1a33fa3b85979a8693f
Teppo Kurki <teppo.kurki@iki.fi>
2024-04-28 09:03:58 +0300

    conceptual edit form rendering

--- packages/server-admin-ui/src/views/DataBrowser/Meta.js
+++ packages/server-admin-ui/src/views/DataBrowser/Meta.js
@@ -1,5 +1,11 @@
+import {
+  faPlusSquare,
+  faSquarePlus,
+  faTrashCan,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import JSONTree from 'react-json-tree'
+import { Col, Form, FormGroup, FormText, Input, Row } from 'reactstrap'
 
 const METAFIELDS = [
   'units',
@@ -37,45 +43,152 @@ export default function Meta({ meta }) {
       delete extraValues[prop]
     }
   }
+
+  const zonesMetaValue = metaValues.find(({ key }) => key === 'zones')
+  const zones = zonesMetaValue ? zonesMetaValue.value : []
   return (
-    <>
-      <ul>
-        {metaValues.map(({ key, value }) => (
-          <li key={key}>
-            {key !== 'zones' && (
-              <>
-                {key}:
-                {typeof value === 'object' ? (
-                  <JSONTree
-                    data={value}
-                    theme="default"
-                    sortObjectKeys
-                    hideRoot
-                  />
-                ) : (
-                  value
-                )}
-              </>
-            )}
-            {key === 'zones' && <Zones zones={value}></Zones>}
-          </li>
-        ))}
-      </ul>
-    </>
+    <Form
+      action=""
+      method="post"
+      encType="multipart/form-data"
+      className="form-horizontal"
+      onSubmit={(e) => {
+        e.preventDefault()
+      }}
+    >
+      {metaValues.map(({ key, value }) => (
+        <>
+          {key !== 'zones' && (
+            <TextMetaFormRow _key={key} value={value}></TextMetaFormRow>
+          )}
+        </>
+      ))}
+      <Zones zones={zones}></Zones>
+      <FontAwesomeIcon
+        className="icon__add_metavalue"
+        icon={faSquarePlus}
+        onClick={(e) => console.log(e)}
+      />
+    </Form>
   )
 }
 
-const Zones = ({zones}) => (
-  <>
-  zones
-  <ul>
-    {zones.map((zone, i) => (
-      <li key={i}>
-        <JSONTree data={zone} theme="default" hideRoot></JSONTree>
-      </li>
-    ))}
-  </ul>
-  </>
+const TextMetaFormRow = ({ _key, value }) => (
+  <FormGroup row>
+    <Col xs="3" md="2" className={'col-form-label'}>
+      <Input
+        type="select"
+        value={_key}
+        name="options.type"
+        onChange={(event) => console.log(event)}
+      >
+        {METAFIELDS.map((fieldName, i) => (
+          <option key={i} value={fieldName}>
+            {fieldName}
+          </option>
+        ))}
+      </Input>
+    </Col>
+    <Col xs="12" md="4">
+      <Input
+        type="text"
+        name="search"
+        onChange={(e) => console.log(e)}
+        value={value}
+      />
+    </Col>
+    <Col>
+      <FontAwesomeIcon
+        className="icon__remove"
+        icon={faTrashCan}
+        onClick={(e) => console.log(e)}
+      />
+    </Col>
+  </FormGroup>
+)
+
+const STATES = ['nominal', 'alert', 'warn', 'alarm', 'emergency']
+const Zone = ({ zone }) => {
+  const { state, lower, upper, message } = zone
+  return (
+    <FormGroup row>
+      <Col xs="2" md="2">
+        <FormText color="muted">Upper</FormText>
+        <Input
+          type="number"
+          name="search"
+          onChange={(e) => console.log(e)}
+          value={lower}
+        />
+      </Col>
+      <Col xs="2" md="2">
+        <FormText color="muted">Upper</FormText>
+        <Input
+          type="number"
+          name="search"
+          onChange={(e) => console.log(e)}
+          value={upper}
+        />
+      </Col>
+      <Col xs="12" md="2">
+        <FormText color="muted">State</FormText>
+        <Input
+          type="select"
+          value={state}
+          name="options.type"
+          onChange={(event) => console.log(event)}
+        >
+          {STATES.map((state, i) => (
+            <option key={i} value={state}>
+              {state}
+            </option>
+          ))}
+        </Input>
+      </Col>
+      <Col xs="3" md="3">
+        <FormText color="muted">Message</FormText>
+        <Input
+          type="text"
+          name="search"
+          onChange={(e) => console.log(e)}
+          value={message}
+        />
+      </Col>
+      <Col xs="2" md="2">
+        <FormText color="muted">Remove</FormText>
+        <FontAwesomeIcon
+          className="icon__remove"
+          icon={faTrashCan}
+          onClick={(e) => console.log(e)}
+        />
+      </Col>
+    </FormGroup>
+  )
+}
+const Zones = ({ zones }) => (
+  <Row>
+    <Col md="2">Zones</Col>
+    <Col md="10">
+      <Form
+        action=""
+        method="post"
+        encType="multipart/form-data"
+        className="form-horizontal"
+        onSubmit={(e) => {
+          e.preventDefault()
+        }}
+      >
+        {zones.map((zone, i) => (
+          <Zone key={i} zone={zone}></Zone>
+        ))}
+      </Form>
+      <FontAwesomeIcon
+        className="icon__remove"
+        icon={faPlusSquare}
+        onClick={(e) => console.log(e)}
+      />
+    </Col>
+  </Row>
 )
 
 const clone = (o) => JSON.parse(JSON.stringify(o))
