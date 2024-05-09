3eaeb2d62519974588c5a7809f5b0c6a18b87311
Scott Bender <scott@scottbender.net>
2024-05-09 16:35:21 -0400

    fix tests failing

--- test/delete.js
+++ test/delete.js
@@ -3,42 +3,45 @@ chai.Should()
 chai.use(require('chai-things'))
 const _ = require('lodash')
 const assert = require('assert')
-const freeport = require('freeport-promise')
-const { startServerP, WsPromiser, sendDelta } = require('./servertestutilities')
+//const freeport = require('freeport-promise')
+//const { startServerP, WsPromiser, sendDelta } = require('./servertestutilities')
+
+import { startServer } from './ts-servertestutilities'
+
 const fetch = require('node-fetch')
 
 describe('Delete Requests', () => {
-  let server, port, deltaUrl, url
+  let doStop, doSendDelta, theHost, doSelfPut, doSelfGetJson, doGet, doCreateWsPromiser
 
   before(async () => {
-    port = await freeport()
-    url = `http://localhost:${port}`
-    deltaUrl = 'http://localhost:' + port + '/signalk/v1/api/_test/delta'
-    server = await startServerP(port, false, { settings: {disableSchemaMetaDeltas: true} })
+    const {
+      createWsPromiser,
+      selfGetJson,
+      selfPutV1,
+      sendDelta,
+      stop,
+      host,
+      selfGetJsonV1,
+      getV1
+    } = await startServer()
+    doStop = stop
+    doSendDelta = sendDelta
+    theHost = host
+    doSelfPut = selfPutV1
+    doSelfGetJson = selfGetJsonV1
+    doGet = getV1
+    doCreateWsPromiser = createWsPromiser
   })
 
   after(async function () {
-    await server.stop()
+    await doStop()
   })
 
-
   it('HTTP delete to unhandled path fails', async function () {
-    await sendDelta({
-      context: 'vesssels.self',
-      updates: [
-        {
-          values: [
-            {
-              path: 'navigation.logTrip',
-              value: 43374
-            }
-          ]
-        }
-      ]
-    }, deltaUrl)
+    await doSendDelta('navigation.logTrip', 43374)
    
     const result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip`,
+      `${theHost}/signalk/v1/api/vessels/self/navigation/logTrip`,
       {
         method: 'DELETE',
         headers: {
@@ -51,48 +54,35 @@ describe('Delete Requests', () => {
   })
 
   it('HTTP successful DELETE', async function () {
-    let result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          value: 'My Log Trip'
-        })
-      }
-    )
+    let result = await doSelfPut('navigation/logTrip/meta/displayName', {
+      value: 'My Log Trip'
+    })
 
     result.status.should.equal(202)
 
-    result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`)
+    result = await doGet('/vessels/self/navigation/logTrip/meta/displayName')
     result.status.should.equal(200)
     let name = await result.json()
     name.should.equal('My Log Trip')
 
     result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`,
+      `${theHost}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`,
       {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json'
         }
       }
-    )
+      )
 
     result.status.should.equal(202)
 
-    result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`)
+    result = await doGet('/vessels/self/navigation/logTrip/meta/displayName')
     result.status.should.equal(404)
   })
-
+  
   it('WS delete to unhandled path fails', async function () {
-    const ws = new WsPromiser(
-      'ws://localhost:' + port + '/signalk/v1/stream?subsribe=none'
-    )
+    const ws = doCreateWsPromiser()
 
     let msg = await ws.nextMsg()
 
@@ -110,31 +100,20 @@ describe('Delete Requests', () => {
     response.statusCode.should.equal(405)
   })
 
+ 
   it('WS successful DELETE', async function () {
-    let result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          value: 'My Log Trip'
-        })
-      }
-    )
+    let result = await doSelfPut('navigation/logTrip/meta/displayName', {
+      value: 'My Log Trip'
+    })
     
     result.status.should.equal(202)
-    
-    result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`)
+
+    result = await doGet('/vessels/self/navigation/logTrip/meta/displayName')
     result.status.should.equal(200)
     let name = await result.json()
     name.should.equal('My Log Trip')
 
-    const ws = new WsPromiser(
-      'ws://localhost:' + port + '/signalk/v1/stream?subsribe=none'
-    )
+    const ws = doCreateWsPromiser()
     
     let msg = await ws.nextMsg()
 
@@ -145,14 +124,15 @@ describe('Delete Requests', () => {
       }
     })
 
-    msg = await ws.nthMessage(4)
+    await ws.nextMsg() //skip the meta delta
+    msg = await ws.nextMsg()
+    console.log(msg)
     msg.should.not.equal('timeout')
     const response = JSON.parse(msg)
     response.should.have.property('statusCode')
-    response.statusCode.should.equal(202)
+    response.statusCode.should.equal(200)
 
-    result = await fetch(
-      `${url}/signalk/v1/api/vessels/self/navigation/logTrip/meta/displayName`)
+    result = await doGet('/vessels/self/navigation/logTrip/meta/displayName')
     result.status.should.equal(404)
   })
 })
