3a8d2d45698a02e0fa4a5f74e657880dd7870270
panaaj <38519157+panaaj@users.noreply.github.com>
2024-12-15 09:49:28 +1030

    fix `path` value. Correct value to contain `autopilots` not `autopilot`

--- src/api/autopilot/openApi.ts
+++ src/api/autopilot/openApi.ts
@@ -3,6 +3,6 @@ import autopilotApiDoc from './openApi.json'
 
 export const autopilotApiRecord = {
   name: 'autopilot',
-  path: '/signalk/v2/api/vessels/self/autopilot',
+  path: '/signalk/v2/api/vessels/self/autopilots',
   apiDoc: autopilotApiDoc as unknown as OpenApiDescription
 }
