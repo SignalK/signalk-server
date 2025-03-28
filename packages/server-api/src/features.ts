0ee6d5cc0d7664d3121a1bf99a51294e3561c1c8
Brandon Keepers <brandon@opensoul.org>
2025-03-28 12:57:14 -0400

    WIP: move plugin docs to typedoc

---
+++ packages/server-api/src/features.ts
@@ -0,0 +1,63 @@
+import { SignalKApiId } from '.'
+
+export interface Features {
+  /**
+   * Returns the available APIs and Plugins.
+   *
+   * _Example:_
+   * ```javascript
+   * let features = app.getFeatures();
+   *
+   * {
+   *   "apis": [
+   *     "resources","course"
+   *   ],
+   *   "plugins": [
+   *     {
+   *       "id": "anchoralarm",
+   *       "name": "Anchor Alarm",
+   *       "version": "1.13.0",
+   *       "enabled": true
+   *     },
+   *     {
+   *       "id": "autopilot",
+   *       "name": "Autopilot Control",
+   *       "version": "1.4.0",
+   *       "enabled": false
+   *     },
+   *     {
+   *       "id": "sk-to-nmea2000",
+   *       "name": "Signal K to NMEA 2000",
+   *       "version": "2.17.0",
+   *       "enabled": false
+   *     },
+   *     {
+   *       "id": "udp-nmea-sender",
+   *       "name": "UDP NMEA0183 Sender",
+   *       "version": "2.0.0",
+   *       "enabled": false
+   *     }
+   *   ]
+   * }
+   * ```
+   *
+   * @param onlyEnabled
+   * - `undefined` (not provided): list all features
+   * - `true`: list only enabled features
+   * - `false`: list only disabled features
+   */
+  getFeatures(onlyEnabled?: boolean): FeatureInfo
+}
+
+/**
+ * Information about the available APIs and Plugins.
+ */
+export interface FeatureInfo {
+  apis: SignalKApiId[]
+  plugins: Array<{
+    id: string
+    name: string
+    version: string
+    enabled: boolean
+  }>
+}
