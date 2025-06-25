5d76e62f67daaa119f045c34f85b86e4eabc4437
Brandon Keepers <brandon@opensoul.org>
2025-06-25 09:05:48 -0400

    fix: restore Node 18 support, recommend 22 (#2018)

---
+++ src/version.ts
@@ -0,0 +1,20 @@
+import { engines } from '../package.json'
+import { satisfies } from 'semver'
+
+const minimumVersion = engines.node
+const recommendedVersion = '22'
+
+export default function checkNodeVersion() {
+  if ('SKIP_NODE_VERSION_CHECK' in process.env) return
+
+  if (!satisfies(process.version, minimumVersion)) {
+    console.error(
+      `The installed version of node (${process.version}) is older than the minimum required version (${minimumVersion}). See https://github.com/SignalK/signalk-server/wiki/Installing-and-Updating-Node.js for more information how to upgrade.`
+    )
+    process.exit(1)
+  } else if (!satisfies(process.version, recommendedVersion)) {
+    console.warn(
+      `The installed version of node (${process.version}) is different than the recommended version (${recommendedVersion}). See https://github.com/SignalK/signalk-server/wiki/Installing-and-Updating-Node.js for more information how to upgrade.`
+    )
+  }
+}
