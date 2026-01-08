d7e676e52c9c649bf130466abfc8d6d59b0c523c
dirkwa <dirkwahrheit@gmail.com>
2026-01-09 07:39:22 +1200

    perf(admin-ui): tree-shake FontAwesome 5 icons
    Use deep imports (@fortawesome/free-solid-svg-icons/faIcon) instead of
    barrel imports to enable tree-shaking. Reduces FA5 icons from 1012KB
    to 17KB.

--- packages/server-admin-ui/src/views/appstore/Apps/WarningBox.js
+++ packages/server-admin-ui/src/views/appstore/Apps/WarningBox.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons/faTriangleExclamation'
 
 export default function WarningBox({ children }) {
   return (
