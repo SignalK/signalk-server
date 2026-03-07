12ca06e14318581c651264dd8d287fe4a72cc4e4
Teppo Kurki <teppo.kurki@iki.fi>
2026-03-07 20:01:52 +0200

    fix: accidental global leak

--- src/discovery.js
+++ src/discovery.js
@@ -217,9 +217,9 @@ module.exports.runDiscovery = function (app) {
             !findWSProvider(data.addresses[0], wsType, data.host, data.port)
           ) {
             debug('discoverSignalkWs found data[' + wsType + ']:', data)
-            const id = providerId = `${wsType}-${data.host}:${data.port} (${data.addresses[0]})`
+            const providerId = `${wsType}-${data.host}:${data.port} (${data.addresses[0]})`
             app.emit('discovered', {
-              id,
+              id: providerId,
               enabled: false,
               pipeElements: [
                 {
